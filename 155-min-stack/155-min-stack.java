e7ee0a54364e1e46cb649793543bd76ce0d8dbbc
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-01 22:44:44 +0530

    Time: 8 ms (51.51%), Space: 48.8 MB (26.70%) - LeetHub

--- 155-min-stack/155-min-stack.java
+++ 155-min-stack/155-min-stack.java
@@ -1,41 +1,28 @@
 class MinStack {
-    long min;
-    Stack<Long> stack;
-
-    public MinStack(){
-        stack=new Stack<>();
-    }
-    
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
     public void push(int x) {
-        if (stack.isEmpty()){
-            stack.push(0L);
+        // only push the old minimum value when the current 
+        // minimum value changes after pushing the new value x
+        if(x <= min){          
+            stack.push(min);
             min=x;
-        }else{
-            stack.push(x-min);//Could be negative if min value needs to change
-            if (x<min) min=x;
         }
+        stack.push(x);
     }
 
     public void pop() {
-        if (stack.isEmpty()) return;
-        
-        long pop=stack.pop();
-        
-        if (pop<0)  min=min-pop;//If negative, increase the min value
-        
+        // if pop operation could result in the changing of the current minimum value, 
+        // pop twice and change the current minimum value to the last minimum value.
+        if(stack.pop() == min) min=stack.pop();
     }
 
     public int top() {
-        long top=stack.peek();
-        if (top>0){
-            return (int)(top+min);
-        }else{
-           return (int)(min);
-        }
+        return stack.peek();
     }
 
     public int getMin() {
-        return (int)min;
+        return min;
     }
 }
 
