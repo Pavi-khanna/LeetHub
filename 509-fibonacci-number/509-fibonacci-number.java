849da1e352bd67ac577865d3b263e524e70b6a0f
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-03 17:04:54 +0530

    Time: 9 ms (34.85%), Space: 41.6 MB (6.91%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -2,13 +2,18 @@ class Solution {
     public int fib(int n) {
         if(n<=1) return n;
         
-        int first=0, second=1, current=0;
+        // 2^n,n
+        return fib(n-1)+fib(n-2);
         
-        for(int i=2; i<=n; i++) {
-            current = first+second;
-            first = second;
-            second = current;
-        }
-        return current;
+        
+        // n,1
+//         int first=0, second=1, current=0;
+        
+//         for(int i=2; i<=n; i++) {
+//             current = first+second;
+//             first = second;
+//             second = current;
+//         }
+//         return current;
     }
 }
