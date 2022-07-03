46848dcd82a29e95ca09efd4d5439ce41b87c196
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-03 22:56:41 +0530

    Time: 2 ms (84.05%), Space: 55.3 MB (72.20%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,28 +1,28 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] arr = new int[nums.length];
-        for(int i=0; i<nums.length; i++) {
-            arr[i] = nums[i]*nums[i];
+        // nlogn, logn
+        // int[] arr = new int[nums.length];
+        // for(int i=0; i<nums.length; i++) {
+        //     arr[i] = nums[i]*nums[i];
+        // }
+        // Arrays.sort(arr);
+        // return arr;
+        
+        // n, n
+        int n=nums.length, left=0, right=n-1;
+        int[] res = new int[n];
+        
+        for(int i=n-1; i>=0; i--) {
+            int square;
+            if(Math.abs(nums[left]) < Math.abs(nums[right])) {
+                square = nums[right];
+                right--;
+            } else {
+                square = nums[left];
+                left++;
+            }
+            res[i] = square*square;
         }
-        Arrays.sort(arr);
-        return arr;
-        
-//         int n = nums.length;
-//         int[] result = new int[n];
-//         int left = 0;
-//         int right = n-1;
-        
-//         for(int i = n-1; i>=0; i--) {
-//             int square;
-//             if(Math.abs(nums[left]) < Math.abs(nums[right])) {
-//                 square = nums[right];
-//                 right--;
-//             } else {
-//                 square = nums[left];
-//                 left++;
-//             }
-//             result[i] = square*square;
-//         }
-//         return result;
+        return res;
     }
 }
