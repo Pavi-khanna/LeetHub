4c606722bda32afc7af25f0c7d3859746c36d307
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-04 13:37:49 +0530

    Added solution - LeetHub

---
+++ Leaders in an array - GFG/leaders-in-an-array.java
@@ -0,0 +1,76 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Array {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		
+		while(t-->0){
+		    
+		    //input size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //inserting elements in the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    StringBuffer str = new StringBuffer();
+		    ArrayList<Integer> res = new ArrayList<Integer>();
+		  
+		    //calling leaders() function
+		    res = obj.leaders(arr, n);
+		    
+		    //appending result to a String
+		    for(int i=0; i<res.size(); i++){
+		        str.append(res.get(i)+" ");
+		    }
+		    
+		    //printing the String
+		    System.out.println(str);
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        
+        // n^2, 1
+        // ArrayList<Integer> res = new ArrayList<>();
+        // for(int i=0; i<n; i++) {
+        //     boolean flag = true;
+        //     for(int j=i+1; j<n; j++) {
+        //         if(arr[i] < arr[j]) {
+        //             flag = false;
+        //         }
+        //     }
+        //     if(flag) res.add(arr[i]);
+        // }
+        // return res;
+        
+        
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        stack.push(arr[n-1]);
+        for(int i=n-2; i>=0; i--) {
+            if(stack.peek() <= arr[i]) stack.push(arr[i]);
+        }
+        while(!stack.isEmpty()) {
+            res.add(stack.pop());
+        }
+        return res;
+    }
+}
