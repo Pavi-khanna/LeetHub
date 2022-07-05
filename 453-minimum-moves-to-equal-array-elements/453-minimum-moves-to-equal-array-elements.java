88276643ce9c2494343f6782f1dc96ec0731e348
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-05 17:08:51 +0530

    Time: 2 ms (88.92%), Space: 53.3 MB (81.50%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.java
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.java
@@ -61,9 +61,11 @@ class Solution {
         // n, 1
         int moves = 0, min = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length; i++) {
-            moves += nums[i];
             min = Math.min(min, nums[i]);
         }
-        return moves - min * nums.length;
+        for(int i=0; i<nums.length; i++) {
+            moves += nums[i]-min;
+        }
+        return moves;
     }
 }
