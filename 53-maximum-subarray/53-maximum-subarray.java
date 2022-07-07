ea83b15c499ae5a6e66a9298267c3e0ec397b16c
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-07 09:35:32 +0530

    Time: 39 ms (5.84%), Space: 85 MB (5.03%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // n^2, 1
+        // int max = Integer.MIN_VALUE;
+        // for(int i=0; i<nums.length; i++) {
+        //     int curMax = 0;
+        //     for(int j=i; j<nums.length; j++) {  // if i+1 -> 1 = int min
+        //         curMax+=nums[j];
+        //         max = Math.max(curMax, max);
+        //     }
+        // }
+        // return max;
+        
+        
+        // n, 1
+        int max = Integer.MIN_VALUE, sum = 0, begin = 0, end = 0, start = 0;
+        for(int i=0; i<nums.length;i++) {
+            sum += nums[i];
+            if(max<sum) {
+                max = sum;
+                start = begin;
+                end = i;
+            }
+            if(sum<0) {
+                sum = 0;
+                begin = i+1;
+            }
+        }
+        System.out.println(Arrays.toString(Arrays.copyOfRange(nums, start, end+1)));
+        return max;
+    }
+}
