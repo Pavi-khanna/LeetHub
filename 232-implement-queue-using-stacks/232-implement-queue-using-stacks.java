6e7205d97e5c0631ca31592ba08926d7ddf3a5d7
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-08 22:21:42 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (89.53%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
@@ -1,7 +1,10 @@
 class MyQueue {
-    private Stack<Integer> s1 = new Stack<>();
-    private Stack<Integer> s2 = new Stack<>();
-    int front;
+    // private Stack<Integer> s1 = new Stack<>();
+    // private Stack<Integer> s2 = new Stack<>();
+    // int front;
+    
+    Stack<Integer> input = new Stack();
+    Stack<Integer> output = new Stack();
 
     public MyQueue() {
         
@@ -14,8 +17,11 @@ class MyQueue {
         // while(!s2.isEmpty()) s1.push(s2.pop());
         
         
-        if(s1.isEmpty()) front = x;
-        s1.push(x);
+        // if(s1.isEmpty()) front = x;
+        // s1.push(x);
+        
+        
+        input.push(x);
     }
     
     public int pop() {
@@ -26,27 +32,40 @@ class MyQueue {
         // return num;
         
         
-        if(s2.isEmpty()) {
-            while(!s1.isEmpty()) s2.push(s1.pop());
-        }
-        return s2.pop();
+        // if(s2.isEmpty()) {
+        //     while(!s1.isEmpty()) s2.push(s1.pop());
+        // }
+        // return s2.pop();
+        
+        
+        peek();
+        return output.pop();
     }
     
     public int peek() {
         // return front;
         
         
-        if (!s2.isEmpty()) {
-            return s2.peek();
-        }
-        return front;
+        // if (!s2.isEmpty()) {
+        //     return s2.peek();
+        // }
+        // return front;
+        
+        
+        if (output.empty())
+            while (!input.empty())
+                output.push(input.pop());
+        return output.peek();
     }
     
     public boolean empty() {
         // return s1.isEmpty();
         
         
-        return s1.isEmpty() && s2.isEmpty();
+        // return s1.isEmpty() && s2.isEmpty();
+        
+        
+        return input.empty() && output.empty();
     }
 }
 
