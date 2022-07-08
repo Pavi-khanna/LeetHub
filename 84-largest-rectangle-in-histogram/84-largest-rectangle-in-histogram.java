6baaa341db63d854307043839a7436206a66f6a4
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-08 22:43:25 +0530

    Time: 450 ms (5.00%), Space: 100.1 MB (12.10%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -0,0 +1,76 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        // n^2, 1
+        // int max = 0, n = heights.length;
+        // for(int i=0; i<n; i++) {
+        //     int minH = Integer.MAX_VALUE;
+        //     for(int j=i; j<n; j++) {
+        //         minH = Math.min(minH, heights[j]);
+        //         max = Math.max(max, minH*(j-i+1));
+        //     }
+        // }
+        // return max;
+        
+        
+        // n, n
+        int n = heights.length, max = Integer.MIN_VALUE;
+        int[] left = leftSmaller(heights), right = rightSmaller(heights), area = new int[n];
+        for(int i=0; i<heights.length; i++) {
+            // System.out.println(left[i] +" "+right[i]);
+            area[i] = heights[i] * (right[i]-left[i]-1);
+            max = Math.max(max, area[i]);
+        }
+        return max;
+    }
+    
+    int[] leftSmaller(int arr[]) {
+        int[] res = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+        int p = 0, pseudoIndex = -1, n = arr.length;
+
+        for(int i=0; i<n; i++) {
+            if(stack.isEmpty()) {
+                res[p++] = pseudoIndex;
+            } else if(!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                res[p++] = stack.peek();
+            } else if(!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                while(stack.size()>0 && arr[stack.peek()] >= arr[i]) {
+                    stack.pop();
+                }
+                if(stack.isEmpty()) res[p++] = pseudoIndex;
+                else res[p++] = stack.peek();
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+    
+    int[] rightSmaller(int arr[]) {
+        int[] res = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+        int p = 0, pseudoIndex = arr.length, n = arr.length;
+        
+        for(int i=n-1; i>=0; i--) {
+            if(stack.isEmpty()) {
+                res[p++] = pseudoIndex;
+            } else if(!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                res[p++] = stack.peek();
+            } else if(!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                while(stack.size()>0 && arr[stack.peek()] >= arr[i]) {
+                    stack.pop();
+                }
+                if(stack.isEmpty()) res[p++] = pseudoIndex;
+                else res[p++] = stack.peek();
+            }
+            stack.push(i);
+        }
+        // reverse
+        int i=0, j=n-1;
+        while(i<=j) {
+            int temp = res[i];
+            res[i++] = res[j];
+            res[j--] = temp;
+        }
+        return res;
+	} 
+}
