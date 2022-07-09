3f1c34724320c398563a4ab7ff33a1bf9dbd8962
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-09 12:25:29 +0530

    Time: 62 ms (12.72%), Space: 55.2 MB (32.83%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.java
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -1,18 +1,47 @@
 class Solution {
-    public int[] nextGreaterElements(int[] nums) {
-        Stack<Integer> stack = new Stack<>();
-        int n = nums.length;
-        int[] nge = new int[n];
+    public int[] nextGreaterElements(int[] arr) {
+//         Stack<Integer> stack = new Stack<>();
+        int n = arr.length;
+//         int[] nge = new int[n];
         
-        for(int i=(2*n-1);i>=0; i--) {
-            while(!stack.isEmpty() && stack.peek() <= nums[i%n]) stack.pop();
+//         for(int i=(2*n-1);i>=0; i--) {
+//             while(!stack.isEmpty() && stack.peek() <= nums[i%n]) stack.pop();
             
-            if(i<n) {
-                if(!stack.isEmpty()) nge[i] = stack.peek(); 
-                else nge[i] = -1;
+//             if(i<n) {
+//                 if(!stack.isEmpty()) nge[i] = stack.peek(); 
+//                 else nge[i] = -1;
+//             }
+//             stack.push(nums[i%n]);
+//         }
+//         return nge;
+        
+        
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        int p = 0;
+        
+        for(int i=2*n-1; i>=0; i--) {
+            if(stack.isEmpty()) {
+                res[(p++)%n] = -1;
+            } else if(!stack.isEmpty() && stack.peek() > arr[i%n]) {
+                res[(p++)%n] = stack.peek();
+            } else if(!stack.isEmpty() && stack.peek() <= arr[i%n]) {
+                while(stack.size()>0 && stack.peek() <= arr[i%n]) {
+                    stack.pop();
+                }
+                if(stack.isEmpty()) res[(p++)%n] = -1;
+                else res[(p++)%n] = stack.peek();
             }
-            stack.push(nums[i%n]);
+            
+            stack.push(arr[i%n]);
         }
-        return nge;
+        // reverse
+        int i=0, j=n-1;
+        while(i<=j) {
+            int temp = res[i];
+            res[i++] = res[j];
+            res[j--] = temp;
+        }
+        return res;
     }
 }
