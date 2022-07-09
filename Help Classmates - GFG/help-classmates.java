8203c7f2c972664d63a93714d5ae78384112839b
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-09 14:05:51 +0530

    Added solution - LeetHub

--- Help Classmates - GFG/help-classmates.java
+++ Help Classmates - GFG/help-classmates.java
@@ -39,49 +39,49 @@ class Solution {
 	public static int[] help_classmate(int arr[], int n) 
 	{ 
 	       // n^2, 1
-	    int[] res = new int[n];
-        int p = 0;
-        for(int i=0; i<arr.length; i++) {
-            for(int j=i+1; j<arr.length; j++) {
-                if(arr[j] < arr[i]) {
-                    res[p++] = arr[j];
-                    break;
-                } else if(j == n-1) {
-                    res[p++] = -1;
-                }
-            }
-        }
-        res[n-1] = -1;
-        return res;
+	//     int[] res = new int[n];
+    //     int p = 0;
+    //     for(int i=0; i<arr.length; i++) {
+    //         for(int j=i+1; j<arr.length; j++) {
+    //             if(arr[j] < arr[i]) {
+    //                 res[p++] = arr[j];
+    //                 break;
+    //             } else if(j == n-1) {
+    //                 res[p++] = -1;
+    //             }
+    //         }
+    //     }
+    //     res[n-1] = -1;
+    //     return res;
     
     
         // n, n
-        // int[] res = new int[n];
-        // Stack<Integer> stack = new Stack<>();
-        // int p = 0;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        int p = 0;
         
-        // for(int i=n-1; i>=0; i--) {
-        //     if(stack.isEmpty()) {
-        //         res[p++] = -1;
-        //     } else if(!stack.isEmpty() && stack.peek() < arr[i]) {
-        //         res[p++] = stack.peek();
-        //     } else if(!stack.isEmpty() && stack.peek() >= arr[i]) {
-        //         while(stack.size()>0 && stack.peek() >= arr[i]) {
-        //             stack.pop();
-        //         }
-        //         if(stack.isEmpty()) res[p++] = -1;
-        //         else res[p++] = stack.peek();
-        //     }
+        for(int i=n-1; i>=0; i--) {
+            if(stack.isEmpty()) {
+                res[p++] = -1;
+            } else if(!stack.isEmpty() && stack.peek() < arr[i]) {
+                res[p++] = stack.peek();
+            } else if(!stack.isEmpty() && stack.peek() >= arr[i]) {
+                while(stack.size()>0 && stack.peek() >= arr[i]) {
+                    stack.pop();
+                }
+                if(stack.isEmpty()) res[p++] = -1;
+                else res[p++] = stack.peek();
+            }
             
-        //     stack.push(arr[i]);
-        // }
-        // // reverse
-        // int i=0, j=n-1;
-        // while(i<=j) {
-        //     int temp = res[i];
-        //     res[i++] = res[j];
-        //     res[j--] = temp;
-        // }
-        // return res;
+            stack.push(arr[i]);
+        }
+        // reverse
+        int i=0, j=n-1;
+        while(i<=j) {
+            int temp = res[i];
+            res[i++] = res[j];
+            res[j--] = temp;
+        }
+        return res;
 	} 
 }
