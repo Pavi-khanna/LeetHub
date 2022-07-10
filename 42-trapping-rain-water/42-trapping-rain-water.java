deb6b691f7a36a605ece35d6bcbd73bccdce98a8
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-10 15:41:53 +0530

    Time: 719 ms (5.01%), Space: 43.4 MB (85.11%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,16 +1,16 @@
 class Solution {
     public int trap(int[] height) {
         // n^2, 1
-//         int ans = 0;
+        int ans = 0;
         
-//         for(int i=1;i<height.length; i++) {
-//             int lh =0, rh=0;
-//             for(int j=i; j>=0; j--) lh = Math.max(lh, height[j]);
-//             for(int j=i; j<height.length;j++) rh = Math.max(rh, height[j]);
+        for(int i=1;i<height.length; i++) {
+            int lh =0, rh=0;
+            for(int j=i; j>=0; j--) lh = Math.max(lh, height[j]);
+            for(int j=i; j<height.length;j++) rh = Math.max(rh, height[j]);
             
-//             ans += Math.min(lh,rh) - height[i];
-//         }
-//         return ans;
+            ans += Math.min(lh,rh) - height[i];
+        }
+        return ans;
         
         // n, n
 //         int n = height.length;
@@ -35,19 +35,19 @@ class Solution {
         
         
         // n,1
-        int left=0, right=height.length-1, sum =0, leftMax = 0, rightMax = 0;
+//         int left=0, right=height.length-1, sum =0, leftMax = 0, rightMax = 0;
         
-        while(left<right) {
-            if(height[left]<height[right]) {
-                if(height[left]>=leftMax) leftMax = height[left];
-                else sum += leftMax - height[left];
-                ++left;
-            } else {
-                if(height[right]>=rightMax) rightMax = height[right];
-                else sum += rightMax - height[right];
-                --right;
-            }
-        }
-        return sum;
+//         while(left<right) {
+//             if(height[left]<height[right]) {
+//                 if(height[left]>=leftMax) leftMax = height[left];
+//                 else sum += leftMax - height[left];
+//                 ++left;
+//             } else {
+//                 if(height[right]>=rightMax) rightMax = height[right];
+//                 else sum += rightMax - height[right];
+//                 --right;
+//             }
+//         }
+//         return sum;
     }
 }
