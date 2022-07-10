013c331c52da9dd66a76969ed03e29b93f8d9ef1
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-10 15:47:18 +0530

    Time: 3 ms (25.40%), Space: 49.1 MB (24.98%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,37 +1,41 @@
 class Solution {
     public int trap(int[] height) {
         // n^2, 1
-        int ans = 0;
+//         int ans = 0;
         
-        for(int i=1;i<height.length; i++) {
-            int lh =0, rh=0;
-            for(int j=i; j>=0; j--) lh = Math.max(lh, height[j]);
-            for(int j=i; j<height.length;j++) rh = Math.max(rh, height[j]);
+//         for(int i=1;i<height.length; i++) {
+//             int lh =0, rh=0;
+//             for(int j=i; j>=0; j--) lh = Math.max(lh, height[j]);
+//             for(int j=i; j<height.length;j++) rh = Math.max(rh, height[j]);
             
-            ans += Math.min(lh,rh) - height[i];
-        }
-        return ans;
+//             ans += Math.min(lh,rh) - height[i];
+//         }
+//         return ans;
         
         // n, n
-//         int n = height.length;
-//         int[] left = new int[n];
-//         int[] right = new int[n];
-//         left[0] = height[0];
-//         for(int i=1; i<n; i++) {
-//             left[i] = Math.max(left[i-1], height[i]);
-//         }
-//         right[n-1] = height[n-1];
-//         for(int i=n-2; i>=0; i--) {
-//             right[i] = Math.max(right[i+1], height[i]);
-//         }
+        int n = height.length;
+        int[] left = new int[n], right = new int[n];
         
-//         int[] waterLevel = new int[n];
-//         int sum = 0;
-//         for(int i=0; i<n; i++) {
-//             waterLevel[i] = Math.min(left[i], right[i]) - height[i];
-//             sum += waterLevel[i];
-//         }
-//         return sum;
+        left[0] = height[0];
+        for(int i=1; i<n; i++) {
+            left[i] = Math.max(left[i-1], height[i]);
+        }
+        int[] waterLevel = new int[n];
+        int sum = 0;
+        right[n-1] = height[n-1];
+        for(int i=n-1; i>=0; i--) {
+            if(i!= n-1) right[i] = Math.max(right[i+1], height[i]);
+            waterLevel[i] = Math.min(left[i], right[i]) - height[i];
+            sum += waterLevel[i];
+        }
+        
+        // int[] waterLevel = new int[n];
+        // int sum = 0;
+        // for(int i=0; i<n; i++) {
+        //     waterLevel[i] = Math.min(left[i], right[i]) - height[i];
+        //     sum += waterLevel[i];
+        // }
+        return sum;
         
         
         // n,1
