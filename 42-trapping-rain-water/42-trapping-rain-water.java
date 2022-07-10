347bf2bf29cd5c2fea9a944799346c4c5d0b9848
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-10 15:49:39 +0530

    Time: 1 ms (99.48%), Space: 43.1 MB (88.18%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -13,37 +13,37 @@ class Solution {
 //         return ans;
         
         // n, n
-        int n = height.length, sum = 0;
-        int[] left = new int[n], right = new int[n], waterLevel = new int[n];
+//         int n = height.length, sum = 0;
+//         int[] left = new int[n], right = new int[n], waterLevel = new int[n];
         
-        left[0] = height[0];
-        for(int i=1; i<n; i++) {
-            left[i] = Math.max(left[i-1], height[i]);
-        }
+//         left[0] = height[0];
+//         for(int i=1; i<n; i++) {
+//             left[i] = Math.max(left[i-1], height[i]);
+//         }
         
-        right[n-1] = height[n-1];
-        for(int i=n-1; i>=0; i--) {
-            if(i!= n-1) right[i] = Math.max(right[i+1], height[i]);
-            waterLevel[i] = Math.min(left[i], right[i]) - height[i];
-            sum += waterLevel[i];
-        }
-        return sum;
+//         right[n-1] = height[n-1];
+//         for(int i=n-1; i>=0; i--) {
+//             if(i!= n-1) right[i] = Math.max(right[i+1], height[i]);
+//             waterLevel[i] = Math.min(left[i], right[i]) - height[i];
+//             sum += waterLevel[i];
+//         }
+//         return sum;
         
         
         // n,1
-//         int left=0, right=height.length-1, sum =0, leftMax = 0, rightMax = 0;
+        int left=0, right=height.length-1, sum =0, leftMax = 0, rightMax = 0;
         
-//         while(left<right) {
-//             if(height[left]<height[right]) {
-//                 if(height[left]>=leftMax) leftMax = height[left];
-//                 else sum += leftMax - height[left];
-//                 ++left;
-//             } else {
-//                 if(height[right]>=rightMax) rightMax = height[right];
-//                 else sum += rightMax - height[right];
-//                 --right;
-//             }
-//         }
-//         return sum;
+        while(left<right) {
+            if(height[left]<height[right]) {
+                if(height[left]>=leftMax) leftMax = height[left];
+                else sum += leftMax - height[left];
+                left++;
+            } else {
+                if(height[right]>=rightMax) rightMax = height[right];
+                else sum += rightMax - height[right];
+                right--;
+            }
+        }
+        return sum;
     }
 }
