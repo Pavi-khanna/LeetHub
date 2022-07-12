4b26fcfc95d95d4d9c718b082a7cd8c6aa0838ab
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-12 23:28:07 +0530

    Time: 3 ms (45.70%), Space: 44.5 MB (61.14%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,10 +1,14 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        // 
+        // n!*n, n, n!+2n
+        // List<List<Integer>> ans = new ArrayList<>();
+        // List<Integer> ds = new ArrayList<>();
+        // boolean[] freq = new boolean[nums.length];
+        // recurPermute(nums, ds, ans, freq);
+        // return ans;
+        
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ds = new ArrayList<>();
-        boolean[] freq = new boolean[nums.length];
-        recurPermute(nums, ds, ans, freq);
+        recurPermute(0, nums, ans);
         return ans;
     }
     
@@ -23,5 +27,28 @@ class Solution {
                 freq[i] = false;
             }
         }
+    }
+    
+    private void recurPermute(int index, int[] nums, List<List<Integer>> ans) {
+        if(index == nums.length) {
+            List<Integer> ds = new ArrayList<>();
+            for(int num : nums) {
+                ds.add(num);
+            }
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        
+        for(int i=index; i<nums.length; i++) {
+            swap(index, i, nums);
+            recurPermute(index+1, nums, ans);
+            swap(index, i, nums);
+        }
+    }
+    
+    public void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
