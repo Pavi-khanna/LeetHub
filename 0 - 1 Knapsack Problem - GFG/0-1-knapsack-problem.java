301ab05ac579e91ba5f4f17824f258b5a08fcc87
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-13 15:05:22 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -59,24 +59,45 @@ class Solution
         //                      knapSack(W, wt, val, n-1));
         //  }
         
+        
+        // int[][] t = new int[n+1][W+1];
+        // for(int i=0; i<n+1; i++) {
+        //     Arrays.fill(t[i], -1);
+        // }
+        // return knapSack(t, W, wt, val, n);
+        
         int[][] t = new int[n+1][W+1];
-        for(int i=0; i<n+1; i++) {
-            Arrays.fill(t[i], -1);
-        }
-        return knapSack(t, W, wt, val, n);
+        return knapSack(t, wt, val);
     } 
     
-    static int knapSack(int[][] t, int W, int wt[], int val[], int n) {
-         if(n==0 || W==0) return 0;
+    // static int knapSack(int[][] t, int W, int wt[], int val[], int n) {
+    //      if(n==0 || W==0) return 0;
          
-         if(t[n][W] != -1) return t[n][W];
+    //      if(t[n][W] != -1) return t[n][W];
          
-         if(wt[n-1] > W) {
-             return t[n][W] = knapSack(t, W, wt, val, n-1);
-         } else {
-             return t[n][W] = Math.max(val[n-1] + knapSack(t, W-wt[n-1], wt, val, n-1),
-                             knapSack(t, W, wt, val, n-1));
+    //      if(wt[n-1] > W) {
+    //          return t[n][W] = knapSack(t, W, wt, val, n-1);
+    //      } else {
+    //          return t[n][W] = Math.max(val[n-1] + knapSack(t, W-wt[n-1], wt, val, n-1),
+    //                          knapSack(t, W, wt, val, n-1));
+    //      }
+    // }
+    
+    static int knapSack(int[][] t, int wt[], int val[]) {
+        int rows = t.length, cols = t[0].length;
+         for(int j=0; j<cols; j++) t[0][j] = 0;
+         for(int i=0; i<rows; i++) t[i][0] = 0;
+         
+         for(int i=1; i<rows; i++) {
+             for(int j=1; j<cols; j++) {
+                 if(wt[i-1] > j) {
+                    t[i][j] = t[i-1][j];
+                 } else {
+                    t[i][j] = Math.max(val[i-1] + t[i-1][j-wt[i-1]], t[i-1][j]);
+                 }
+             }
          }
+         return t[rows-1][cols-1];
     }
 }
 
