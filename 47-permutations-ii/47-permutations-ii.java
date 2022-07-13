4397d370861ec2152fd6700eebe103da7348d6cf
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-13 12:21:49 +0530

    Time: 40 ms (22.56%), Space: 42.8 MB (97.13%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,12 +1,40 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         Set<List<Integer>> ans = new HashSet<>();
-        recurPermute(0, nums, ans);
+        List<Integer> ds = new ArrayList<>();
+        boolean[] freq = new boolean[nums.length];
+        recurPermute(nums, ds, ans, freq);
         List<List<Integer>> list = new ArrayList<>();
         for(List<Integer> num : ans) {
             list.add(new ArrayList<>(num));
         }
         return list;
+        
+        
+        // Set<List<Integer>> ans = new HashSet<>();
+        // recurPermute(0, nums, ans);
+        // List<List<Integer>> list = new ArrayList<>();
+        // for(List<Integer> num : ans) {
+        //     list.add(new ArrayList<>(num));
+        // }
+        // return list;
+    }
+    
+    private void recurPermute(int[] nums, List<Integer> ds,Set<List<Integer>> ans, boolean[] freq) {
+        if(ds.size() == nums.length) {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        
+        for(int i=0; i<nums.length; i++) {
+            if(!freq[i]) {
+                ds.add(nums[i]);
+                freq[i] = true;
+                recurPermute(nums, ds, ans, freq);
+                ds.remove(ds.size()-1);
+                freq[i] = false;
+            }
+        }
     }
     
     private void recurPermute(int index, int[] nums, Set<List<Integer>> ans) {
