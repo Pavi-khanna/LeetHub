994019125b95561060fc471352e3253720855643
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-15 15:27:08 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.java
+++ Subset Sum Problem - GFG/subset-sum-problem.java
@@ -33,33 +33,62 @@ class GFG
 //User function Template for Java
 
 class Solution{
-    public Boolean isSubsetSum(int N, int arr[], int sum){
+    public Boolean isSubsetSum(int n, int arr[], int sum){
         // return sum(arr, N, sum, 0, 0, new ArrayList<>(), new ArrayList<>());
         
-        boolean[][] t = new boolean[N+1][sum+1];
-        return sum(t, arr);
+        //  if(n==0 && sum!=0) return false;
+        //  if(n==0) return true;
+         
+        //  if(arr[n-1] > sum) {
+        //      return isSubsetSum(n-1, arr, sum);
+        //  } else {
+        //      return isSubsetSum(n-1, arr, sum-arr[n-1]) || isSubsetSum(n-1, arr, sum);
+        //  }
+        
+        int[][] t = new int[n+1][sum+1];
+        for(int i=0; i<n+1; i++) {
+            Arrays.fill(t[i], -1);
+        }
+        return isSubsetSum(t, sum, arr, n)==1 ? true : false;
+        
+        // boolean[][] t = new boolean[N+1][sum+1];
+        // return sum(t, arr);
     }
     
-    public boolean sum(int arr[], int n, int sum, int i, int s, List<Integer> ds, List<List<Integer>> ans) {
-        if(i == n) {
-           if(s == sum) {
-               // System.out.println(ds);
-               // ans.add(new ArrayList<>(ds));
-               return true;
-           }
-           else return false;
-        }
-        
-        ds.add(arr[i]);
-        s+=arr[i];
-        if(sum(arr,n,sum,i+1,s,ds,ans)) return true;
-        ds.remove(ds.size()-1);
-        s-=arr[i];
-        
-        if(sum(arr,n,sum,i+1,s,ds,ans)) return true;
-        
-        return false;
+     static int isSubsetSum(int[][] t, int sum, int arr[], int n) {
+         if(n==0 && sum!=0) return 0;
+         if(n==0) return 1;
+         
+         if(t[n][sum] != -1) return t[n][sum];
+         
+         if(arr[n-1] > sum) {
+             return t[n][sum] = isSubsetSum(t, sum, arr, n-1);
+         } else {
+             return t[n][sum] = (isSubsetSum(t, sum-arr[n-1], arr, n-1)==1 || 
+             isSubsetSum(t, sum, arr, n-1)==1) ? 1 : 0;
+         }
     }
+    
+    // public boolean sum(int arr[], int n, int sum, int i, int s, List<Integer> ds, List<List<Integer>> ans) {
+    //     if(i == n) {
+    //       if(s == sum) {
+    //           // System.out.println(ds);
+    //           // ans.add(new ArrayList<>(ds));
+    //           return true;
+    //       }
+    //       else return false;
+    //     }
+        
+    //     ds.add(arr[i]);
+    //     s+=arr[i];
+    //     if(sum(arr,n,sum,i+1,s,ds,ans)) return true;
+    //     ds.remove(ds.size()-1);
+    //     s-=arr[i];
+        
+    //     if(sum(arr,n,sum,i+1,s,ds,ans)) return true;
+        
+    //     return false;
+    // }
     
     public boolean sum(boolean[][] t,int arr[]) {
         int rows = t.length, cols = t[0].length;
