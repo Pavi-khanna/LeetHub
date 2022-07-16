fbe33033c3156fd959f50dfffc58fe9a070d40f3
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-16 17:11:38 +0530

    Time: 514 ms (35.26%), Space: 43.4 MB (46.03%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -3,12 +3,17 @@ class Solution {
         int totalSum = 0;
         for(int num : nums) totalSum += num;
         int sum = (target+totalSum)/2;
-        if((target+totalSum)%2!=0) return 0;
-        return findTargetSumWays(nums.length, nums, sum);
+        if((target+totalSum)%2!=0 || sum<0) return 0;
+        // return findTargetSumWays(nums.length, nums, sum);
+        int t[][] = new int[nums.length+1][sum+1];
+        for(int i=0; i<nums.length+1; i++) {
+            Arrays.fill(t[i], -1);
+        }
+        return findTargetSumWays(t, nums.length, nums, sum);
     }
     
     public int findTargetSumWays(int n, int[] arr, int sum) {
-        if(n==0) {
+         if(n==0) {
              if(sum!=0) return 0;
              else return 1;
          }
@@ -17,6 +22,22 @@ class Solution {
              return findTargetSumWays(n-1, arr, sum);
          } else {
              return findTargetSumWays(n-1, arr, sum-arr[n-1]) + findTargetSumWays(n-1, arr, sum);
+         }
+    }
+    
+    public int findTargetSumWays(int[][] t, int n, int[] arr, int sum) {
+        if(n==0) {
+             if(sum!=0) return 0;
+             else return 1;
+         }
+        
+        if(t[n][sum] != -1) return t[n][sum];
+         
+         if(arr[n-1] > sum) {
+             return t[n][sum] = findTargetSumWays(n-1, arr, sum);
+         } else {
+             return t[n][sum] = findTargetSumWays(n-1, arr, sum-arr[n-1]) +
+                 findTargetSumWays(n-1, arr, sum);
          }
     }
 }
