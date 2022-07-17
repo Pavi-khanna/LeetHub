fd3e179cb3d7f0e0a673ea1c5f9828faaa3261bb
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-17 18:12:34 +0530

    Time: 13 ms (90.71%), Space: 46.6 MB (35.27%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -6,10 +6,13 @@ class Solution {
         // return longestCommonSubsequence(t1, t2, text1.length(), text2.length());
         
         int[][] t = new int[text1.length()+1][text2.length()+1];
-        for(int i=0; i<text1.length()+1; i++) {
-            Arrays.fill(t[i],-1);
-        }
-        return longestCommonSubsequence(t, t1, t2, text1.length(), text2.length());
+        // for(int i=0; i<text1.length()+1; i++) {
+        //     Arrays.fill(t[i],-1);
+        // }
+        // return longestCommonSubsequence(t, t1, t2, text1.length(), text2.length());
+        
+        
+        return longestCommonSubsequence(t, t1, t2);
     }
     
     public int longestCommonSubsequence(char[] t1, char[] t2, int n, int m) {
@@ -36,19 +39,19 @@ class Solution {
         }
     }
     
-//     public int longestCommonSubsequence(int[][] t, char[] t1, char[] t2) {
-//         for(int i=0; i<t.length; i++) t[i][0] = 0;
-//         for(int i=0; i<t[0].length; i++) t[0][i] = 0;
+    public int longestCommonSubsequence(int[][] t, char[] t1, char[] t2) {
+        for(int i=0; i<t.length; i++) t[i][0] = 0;
+        for(int i=0; i<t[0].length; i++) t[0][i] = 0;
                 
-//         for(int i=1; i<t.length; i++) {
-//             for(int j=1; j<t[0].length; j++) {
-//                 if(t1[i-1] == t2[j-1]) {
-//                     t[i][j] = 1+t[i-1][j-1];
-//                 } else {
-//                     t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
-//                 }
-//             }
-//         }
-//         return t[t.length-1][t[0].length-1];
-//     }
+        for(int i=1; i<t.length; i++) {
+            for(int j=1; j<t[0].length; j++) {
+                if(t1[i-1] == t2[j-1]) {
+                    t[i][j] = 1+t[i-1][j-1];
+                } else {
+                    t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
+                }
+            }
+        }
+        return t[t.length-1][t[0].length-1];
+    }
 }
