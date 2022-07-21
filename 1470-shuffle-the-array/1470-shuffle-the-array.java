722a7bfe44b9a8ed7e3b532ee6ea93698694e38a
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-21 15:11:13 +0530

    Time: 1 ms (63.12%), Space: 45.4 MB (68.25%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int[] even = new int[nums.length/2];
-        for(int i=0; i<even.length; i++) {
-            even[i] = nums[i];
+        // n, n
+        int[] ans = new int[ 2 * n];
+        for (int i = 0; i < n; ++i) {
+            ans[2 * i] = nums[i];
+            ans[2 * i + 1] = nums[i + n];
         }
+        return ans;
         
-        int j = nums.length/2;
-        for(int i=1; i<nums.length && j<nums.length; i+=2) {
-            nums[i-1] = even[j-n];
-            nums[i] = nums[j];
-            j++;
-        }
-        return nums;
+        
+        // n,1
     }
 }
