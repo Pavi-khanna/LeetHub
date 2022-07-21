b611578fc4842342f18b571072540fe650a46773
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-21 12:45:34 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (12.72%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -1,22 +1,35 @@
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
-        int i = 0, j = 0;
-        int sumTotal = 0, n = arr.length;
+        // n^2, 1
+//         int i = 0, j = 0;
+//         int sumTotal = 0, n = arr.length;
                 
-        for(int window=1; window<=arr.length; window+=2) {
-            int sum = 0;
-            j=0; i=0;
-            while(j < n) {
-                sum += arr[j];
+//         for(int window=1; window<=arr.length; window+=2) {
+//             int sum = 0;
+//             j=0; i=0;
+//             while(j < n) {
+//                 sum += arr[j];
             
-                if(j-i+1 == window) {
-                    sumTotal+=sum;
-                    sum -= arr[i];
-                    i++;
-                }
-                j++;
-            }
+//                 if(j-i+1 == window) {
+//                     sumTotal+=sum;
+//                     sum -= arr[i];
+//                     i++;
+//                 }
+//                 j++;
+//             }
+//         }
+//         return sumTotal;
+        
+        
+ //        1  4  2  5  3
+ // times :3  4  5  4  3
+ // total :3 16 10 20  9 = 58
+        // odd - k+1/2 & even - k/2 where k = no of subarrays
+        // n, 1
+        int sum=0, n=arr.length;
+        for(int i=0; i<n; i++) {
+            sum += (((i+1)*(n-i)+1)/2)*arr[i];
         }
-        return sumTotal;
+        return sum;
     }
 }
