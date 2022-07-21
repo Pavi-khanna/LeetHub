493952b6e90b95db9fed02bb93db98f9f5341b06
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-21 15:35:57 +0530

    Time: 1 ms (63.12%), Space: 45.3 MB (73.40%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,14 +1,30 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         // n, n
-        int[] ans = new int[ 2 * n];
-        for (int i = 0; i < n; ++i) {
-            ans[2 * i] = nums[i];
-            ans[2 * i + 1] = nums[i + n];
-        }
-        return ans;
+        // int[] ans = new int[ 2 * n];
+        // for (int i = 0; i < n; ++i) {
+        //     ans[2 * i] = nums[i];
+        //     ans[2 * i + 1] = nums[i + n];
+        // }
+        // return ans;
         
         
         // n,1
+        for(int i = 0; i < n; i++){
+            nums[i] += nums[n + i] * 10000;
+        }
+        
+        //System.out.println(Arrays.toString(nums));
+        
+        for(int i = n - 1; i >= 0; i--){
+            nums[2 * i + 1] = nums[i] / 10000;
+            //System.out.println(Arrays.toString(nums));
+            nums[2 * i] = nums[i] % 10000;
+            //System.out.println(Arrays.toString(nums));
+        }
+        
+        
+        
+        return nums;
     }
 }
