ddfe5a56ef7504c9dc856228f644a087c788ff9e
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-21 20:40:28 +0530

    Time: 3 ms (41.78%), Space: 41.7 MB (43.09%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,18 +1,39 @@
 class Solution {
-    public int lastStoneWeight(int[] stones) {        
-        PriorityQueue<Integer> q = new PriorityQueue<>(Comparator.reverseOrder());
-        for(int i=0; i<stones.length; i++) {
-            q.add(stones[i]);
+    public int lastStoneWeight(int[] stones) {
+        List<Integer> stoneList = new ArrayList<>();
+        for (int stone : stones) {
+            stoneList.add(stone);
         }
-        
-        while(q.size()>1) {
-            int first = q.remove();
-            int second = q.remove();
-            
-            if (first != second) {
-                q.add(first - second);
+        Collections.sort(stoneList);
+
+        while (stoneList.size() > 1) {
+            int stone1 = stoneList.remove(stoneList.size() - 1);
+            int stone2 = stoneList.remove(stoneList.size() - 1);
+
+            if (stone1 != stone2) {
+                if(stone1>stone2) stoneList.add(stone1-stone2);
+                else if(stone1<stone2) stoneList.add(stone2-stone1);
             }
+            Collections.sort(stoneList);
         }
-        return q.isEmpty() ? 0 : q.remove();
+
+        return !stoneList.isEmpty() ? stoneList.remove(0) : 0;
+        
+        
+        // nlogn, logn/1
+//         PriorityQueue<Integer> q = new PriorityQueue<>(Comparator.reverseOrder());
+//         for(int i=0; i<stones.length; i++) {
+//             q.add(stones[i]);
+//         }
+        
+//         while(q.size()>1) {
+//             int first = q.remove();
+//             int second = q.remove();
+            
+//             if (first != second) {
+//                 q.add(first - second);
+//             }
+//         }
+//         return q.isEmpty() ? 0 : q.remove();
     }
 }
