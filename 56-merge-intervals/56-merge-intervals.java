c9252198fec66ccf6bb6b171ee9a773623066c82
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-23 00:37:36 +0530

    Time: 11 ms (81.93%), Space: 55.9 MB (8.25%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -9,9 +9,11 @@ class Solution {
 //         for(int i=0; i<n; i++) {
 //             int start = intervals[i][0], end = intervals[i][1];
 //             if(!res.isEmpty()) {
+//                 // not mutually exclusive
 //                 if(res.get(res.size()-1)[1] >= start) continue;   // res->1,3 | curr->2,4 => 3>2
 //             }
 //             for(int j=i+1; j<n; j++) {
+//                 // not mutually exclusive
 //                 if(end >= intervals[j][0]) {                           // 1,4  >  3,5
 //                     end = Math.max(end, intervals[j][1]);              // ec : 1,4 & 2,3
 //                 } else 
@@ -30,7 +32,7 @@ class Solution {
         int end = intervals[0][1];
         
         for(int[] i: intervals) {
-            if(i[0]<=end) { end = Math.max(end,i[1]); }       // 1,3 & 2,4
+            if(end>=i[0]) { end = Math.max(end,i[1]); }       // 1,3 & 2,4
             else {
                 res.add(new int[]{start,end});                // 1,3 & 5,6
                 start = i[0]; end = i[1];
