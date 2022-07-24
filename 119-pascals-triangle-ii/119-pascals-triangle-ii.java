1bfccaf372839bf822017a696d77ade19dcbe1b3
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-24 21:09:18 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (94.67%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -4,6 +4,7 @@ class Solution {
         long column = 1;
         
         for(int i=0; i<=rowIndex; i++) {
+            // System.out.println((int)column);
             res.add((int)column);
             column = (column * (rowIndex-i)) / (i+1);// if int then cannot store big nos and flips to -ive
         }
