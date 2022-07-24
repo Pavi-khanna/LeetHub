0b0a7dc1e755150d736901567b6d8978b6f38444
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-24 20:22:11 +0530

    Time: 1 ms (97.38%), Space: 42 MB (94.99%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,27 +1,28 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int total = 0, valley = Integer.MAX_VALUE, peak = valley;
-        
-        for(int i=0;i<prices.length;i++) {
-            if(prices[i]<peak) {
-                total+=peak-valley;
+        // n, 1
+//         int total = 0, valley = Integer.MAX_VALUE, peak = valley;
+//         for(int i=0;i<prices.length;i++) {
+//             if(prices[i]<peak) {
+//                 total+=peak-valley;
                 
-                valley = prices[i];
-                peak = valley;
-            } else {
-                peak = prices[i];
-            }
-        }
-        total+=peak-valley;
-        return total;
-        
-        
-//         int total = 0;
-//         for(int i=1;i<prices.length;i++) {
-//             if(prices[i]>prices[i-1]) {
-//                 total+=prices[i] - prices[i-1];
+//                 valley = prices[i];
+//                 peak = valley;
+//             } else {
+//                 peak = prices[i];
 //             }
 //         }
+//         total+=peak-valley;
 //         return total;
+        
+        
+        // n, 1
+        int total = 0;
+        for(int i=1;i<prices.length;i++) {
+            if(prices[i]>prices[i-1]) {
+                total+=prices[i] - prices[i-1];
+            }
+        }
+        return total;
     }
 }
