8ce1065a9713cf654aa929b2d594f74d7eb46eeb
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-25 18:00:27 +0530

    Time: 2 ms (93.25%), Space: 59.2 MB (88.18%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int maxProfit(int[] prices) {
+    public int maxProfit(int[] nums) {
         // n^2, 1
         // int profit = 0;
         // for(int i=0; i<prices.length; i++) {
@@ -11,44 +11,36 @@ class Solution {
         
         
         // n, 1
-        int minPrice = Integer.MAX_VALUE, maxProfit = 0;
-        for(int i=0;i<prices.length;i++) {
-            int profit = prices[i]-minPrice;
-            if(prices[i]<minPrice) minPrice = prices[i];
-            else if(profit > maxProfit) maxProfit = profit;
+        // int minPrice = Integer.MAX_VALUE, maxProfit = 0;
+        // for(int i=0;i<prices.length;i++) {
+        //     int profit = prices[i]-minPrice;
+        //     if(prices[i]<minPrice) minPrice = prices[i];
+        //     else if(profit > maxProfit) maxProfit = profit;
+        // }
+        // return maxProfit;
+        
+        
+        // kadane
+        
+        // int max = 0;                                 // change here
+        // for(int i=0; i<nums.length; i++) {
+        //     int curMax = 0;
+        //     for(int j=i+1; j<nums.length; j++) {     // change here
+        //         curMax=nums[j]-nums[i];              // change here
+        //         max = Math.max(curMax, max);
+        //     }
+        // }
+        // return max;
+        
+        
+        int max = 0, sum = 0;                            // change here
+        for(int i=1; i<nums.length;i++) {                // change here
+            sum += nums[i]-nums[i-1];                    // change here
+            if(sum<0) sum = 0;
+            if(max<sum) {
+                max = sum;
+            }
         }
-        return maxProfit;
-        
-        // int max = Integer.MIN_VALUE;
-        // for(int i=0; i<nums.length; i++) {
-        //     int curMax = 0;
-        //     for(int j=i; j<nums.length; j++) {  // if i+1 -> i/p 1 => o/p int min -2147483648
-        //         curMax+=nums[j];
-        //         max = Math.max(curMax, max);
-        //     }
-        // }
-        // return max;
-        
-        // int max = 0;
-        // for(int i=0; i<nums.length; i++) {
-        //     int curMax = 0;
-        //     for(int j=i+1; j<nums.length; j++) {  // if i+1 -> i/p 1 => o/p int min -2147483648
-        //         curMax=nums[j]-nums[i];
-        //         max = Math.max(curMax, max);
-        //     }
-        // }
-        // return max;
-        
-        // int max = 0, sum = 0;
-        // for(int i=1; i<nums.length;i++) {
-        //     sum += nums[i]-nums[i-1];
-        //     if(max<sum) {
-        //         max = sum;
-        //     }
-        //     if(sum<0) {
-        //         sum = 0;
-        //     }
-        // }
-        // return max;
+        return max;
     }
 }
