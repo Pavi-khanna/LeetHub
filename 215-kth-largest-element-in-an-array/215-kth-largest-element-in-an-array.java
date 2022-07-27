bf35c65d75e7a6cfa2f6f9dcec779f2229d34000
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-27 20:17:17 +0530

    Time: 26 ms (10.00%), Space: 69.5 MB (6.06%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -6,17 +6,48 @@ class Solution {
         
         
         // nlogk, k
-        PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
-        List<Integer> list = new ArrayList<>();
+//         PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
+//         List<Integer> list = new ArrayList<>();
         
-        // keep k largest elements in the heap
-        for (int n: nums) {
-          heap.add(n);
-          if (heap.size() > k) heap.poll(); 
-        }
-        return heap.peek();
+//         // keep k largest elements in the heap
+//         for (int n: nums) {
+//           heap.add(n);
+//           if (heap.size() > k) heap.poll();
+//           // System.out.println(heap);
+//         }
+//         return heap.peek();
         
         
         // quickselect
+        return kthLargest(nums, 0, nums.length-1, nums.length-k+1);
+    }
+    
+    public int kthLargest(int[] arr, int low, int high, int k) {
+        int partition = partition(arr, low, high);
+        if(partition == k-1) return arr[partition];
+        else if (partition < k - 1)
+            return kthLargest(arr, partition + 1, high, k);
+        else
+            return kthLargest(arr, low, partition - 1, k);
+    }
+    
+    public int partition(int[] arr, int low, int high) {
+        int pivot = arr[high], pivotloc = low;
+        for (int i = low; i <= high; i++) {
+            // inserting elements of less value
+            // to the left of the pivot location
+            if (arr[i] < pivot) {
+                int temp = arr[i];
+                arr[i] = arr[pivotloc];
+                arr[pivotloc] = temp;
+                pivotloc++;
+            }
+        }
+        // swapping pivot to the final pivot location
+        int temp = arr[high];
+        arr[high] = arr[pivotloc];
+        arr[pivotloc] = temp;
+  
+        return pivotloc;
     }
 }
