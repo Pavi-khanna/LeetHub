8e213e6fd4b659813ac278aeac1faf22c1bac208
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-27 21:02:06 +0530

    Time: 1 ms (38.49%), Space: 47.2 MB (58.33%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        boolean isFirst = false;
+        int first=-1, last=-1;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i]==target) {
+                if(!isFirst) { first = i; isFirst=true; }
+                last = i;
+            }
+        }
+        return new int[]{first, last};
+        
+        
+        // int first = first(nums, target, true);
+        // int last = first(nums, target, false);
+        // return new int[]{first, last};
+    }
+    
+    public int first(int[] nums, int target, boolean isFirst) {
+        int start = 0, end = nums.length-1, first=-1;
+        int mid = start + (end-start)/2;
+        
+        while(start <= end) {
+            mid = start + (end-start)/2;
+            
+            if(nums[mid]==target) {
+                first = mid;
+                if(isFirst) end = mid-1;
+                else start = mid+1;
+            } else if(nums[mid]<target) {
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return first;
+    }
+    
+//     public int last(int[] nums, int target) {
+//         int start = 0, end = nums.length-1, last=-1;
+//         int mid = start + (end-start)/2;
+        
+//         while(start <= end) {
+//             mid = start + (end-start)/2;
+            
+//             if(nums[mid]==target) {
+//                 last = mid;
+//                 start = mid+1;
+//             } else if(nums[mid]<target) {
+//                 start = mid+1;
+//             } else {
+//                 end = mid-1;
+//             }
+//         }
+//         return last;
+//     }
+}
