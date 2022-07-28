b7bee35b16b459607fe041d37eaaf6d3cfd34e36
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-28 12:04:00 +0530

    Time: 1 ms (80.97%), Space: 43.9 MB (26.74%) - LeetHub

---
+++ 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int start = 0, end = nums.length-1, mid = start + (end-start)/2;
+        
+        if(nums == null || nums.length == 0) return -1;
+        
+        while(start <= end) {
+            mid = start + (end-start)/2;
+            
+            // we just have to go to the rotated side doesn't matter if its on the left or right bcz the min                element lies on that side only and accd to the graph of rotated sorted array the unsorted                    side is the rotated one.
+            //                      /\    
+            //                     /  \  /
+            // array graph =           \/
+            
+            
+            // left side
+            if(nums[mid] < nums[end]) {
+                end = mid;
+            } else if(nums[mid] > nums[end]){ // right side
+                start = mid + 1;
+            } else {
+                end -= 1;
+            }
+        }
+        return nums[mid];
+    }
+}
