f54b32d63ce202635df77f744b8ccaed50fae8eb
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-07-29 15:43:28 +0530

    Time: 7 ms (36.03%), Space: 44.3 MB (43.30%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -3,21 +3,53 @@ class Solution {
         int n = nums2.length;
         int[] res = new int[n];
         int[] ans = new int[nums1.length];
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
         int p = 0, k = 0;
-        for(int i=0; i<nums2.length; i++) {
-            for(int j=i+1; j<nums2.length; j++) {
-                if(nums2[j] > nums2[i]) {
-                    res[p++] = nums2[j];
-                    break;
-                } else if(j == n-1) {
-                    res[p++] = -1;
-                }
-            }
-        }
-        res[n-1] = -1;
         
-        for(int i=0; i<nums1.length; i++) {
-            int j = findIndex(nums2, nums1[i]);
+        
+        // for(int i=0; i<nums2.length; i++) {
+        //     for(int j=i+1; j<nums2.length; j++) {
+        //         if(nums2[j] > nums2[i]) {
+        //             res[p++] = nums2[j];
+        //             break;
+        //         } else if(j == n-1) {
+        //             res[p++] = -1;
+        //         }
+        //     }
+        // }
+        // res[n-1] = -1;
+        
+        
+        for(int i=n-1; i>=0; i--) {
+            if(stack.isEmpty()) {
+                res[p++] = -1;
+            } else if(!stack.isEmpty() && stack.peek() > nums2[i]) {
+                res[p++] = stack.peek();
+            } else if(!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                while(stack.size()>0 && stack.peek() <= nums2[i]) {
+                    stack.pop();
+                }
+                if(stack.isEmpty()) res[p++] = -1;
+                else res[p++] = stack.peek();
+            }
+            
+            stack.push(nums2[i]);
+            map.put(nums2[i], i);
+        }
+        // reverse
+        int i=0, j=n-1;
+        while(i<=j) {
+            int temp = res[i];
+            res[i++] = res[j];
+            res[j--] = temp;
+        }
+        
+        for(i=0; i<nums1.length; i++) {
+            
+            // int j = findIndex(nums2, nums1[i]);
+            
+            j = map.get(nums1[i]);
             if(nums1[i] == nums2[j]) {
                 ans[i] = res[j];
             }
@@ -25,6 +57,7 @@ class Solution {
         return ans;
     }
     
+    // or a hashmap
     public int findIndex(int[] nums, int ele) {
         for(int i=0; i<nums.length; i++) {
             if(ele == nums[i]) return i;
