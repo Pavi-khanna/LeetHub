704282ef8458b788763366379cec0bb896f1d124
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-08-02 14:45:18 +0530

    Time: 7 ms (13.69%), Space: 43.7 MB (76.85%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,4 +1,22 @@
 class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        // 2^n*mlogm (m = 2^n) => n*2^2n, 
+        Arrays.sort(nums);
+        Set<List<Integer>> set = printAllSubSequences(0, new ArrayList<>(), nums, new HashSet<>());
+        List<List<Integer>> list = new ArrayList<>();
+        for(List<Integer> num : set) {
+            list.add(new ArrayList<>(num));
+        }
+        return list;
+        
+        
+        // n*2^n, n
+        // Arrays.sort(nums);
+        // List<List<Integer>> ans = new ArrayList<>();
+        // findSubsets(0, nums, new ArrayList<>(), ans);
+        // return ans;
+    }
+    
     private void findSubsets(int index, int[] nums, List<Integer> ds, List<List<Integer>> ans) {
         ans.add(new ArrayList<>(ds));
         for(int i=index; i<nums.length;i++) {
@@ -9,10 +27,17 @@ class Solution {
         }
     }
     
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        findSubsets(0, nums, new ArrayList<>(), ans);
-        return ans;
+    private Set<List<Integer>> printAllSubSequences(int i, List<Integer> prevSubSet, int[] nums, Set<List<Integer>> res) {
+        if(i==nums.length){
+            res.add(new ArrayList<>(prevSubSet));
+            return res;
+        }
+                
+        prevSubSet.add(nums[i]);
+        printAllSubSequences(i+1, prevSubSet, nums, res);
+        prevSubSet.remove(prevSubSet.size()-1);
+        printAllSubSequences(i+1, prevSubSet, nums, res);
+        
+        return res;
     }
 }
