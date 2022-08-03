48f52e393518dabddc34b10ecdcde353fba7ddad
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-08-03 14:36:32 +0530

    Time: 1 ms (89.35%), Space: 43.3 MB (53.44%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,4 +1,13 @@
 class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        // n*2^n, n
+        // return printAllSubSequences(0, new ArrayList<>(), nums, new ArrayList<>());
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        findSubsets(0, nums, new ArrayList<>(), ans);
+        return ans;
+    }
+    
     public List<List<Integer>> printAllSubSequences(int i, List<Integer> prevSubSet, int[] nums, List<List<Integer>> res) {
         if(i==nums.length){
             res.add(new ArrayList<>(prevSubSet));
@@ -13,8 +22,12 @@ class Solution {
         return res;
     }
     
-    public List<List<Integer>> subsets(int[] nums) {
-        // n*2^n, n
-        return printAllSubSequences(0, new ArrayList<>(), nums, new ArrayList<>());
+    private void findSubsets(int index, int[] nums, List<Integer> ds, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(ds));
+        for(int i=index; i<nums.length;i++) {
+            ds.add(nums[i]);
+            findSubsets(i+1, nums, ds, ans);
+            ds.remove(ds.size()-1);
+        }
     }
 }
