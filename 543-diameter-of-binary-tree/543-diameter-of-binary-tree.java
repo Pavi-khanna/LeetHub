68df5ed1453966f1866acf11c4311764f13c2c94
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-08-11 22:46:44 +0530

    Time: 10 ms (15.21%), Space: 43.2 MB (64.16%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -15,21 +15,42 @@
  */
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        int[] diameter = {Integer.MIN_VALUE};
-        height(root,diameter);
-        return diameter[0]-1;
+        int max = 0;
+        if(root == null) return 0;
+        
+        int lh = heightBrute(root.left);
+        int rh = heightBrute(root.right);
+        
+        max = Math.max(max, lh+rh);
+        // tc of both the subtrees as well
+        int l = diameterOfBinaryTree(root.left);
+        int r = diameterOfBinaryTree(root.right);
+        
+        return Math.max(max, Math.max(l,r));
+        
+        
+        // int[] diameter = new int[1];
+        // height(root,diameter);
+        // return diameter[0];
     }
     
     public int height(TreeNode node, int[] diameter) {
+        if(node==null) return 0;
+        
+        int lh = height(node.left, diameter);
+        int rh = height(node.right, diameter);
+        
+        diameter[0] = Math.max(diameter[0],lh+rh);
+
+        return 1+Math.max(lh,rh);
+    }
+    
+    public int heightBrute(TreeNode node) {
         if(node == null) return 0;
         
-        int l = height(node.left, diameter);
-        int r = height(node.right, diameter);
+        int left = heightBrute(node.left);
+        int right = heightBrute(node.right);
         
-        int temp = 1+ Math.max(l,r);
-        int ans = Math.max(temp, 1+l+r);
-        diameter[0] = Math.max(diameter[0], ans);
-        
-        return temp;
+        return 1 + Math.max(left, right);
     }
 }
