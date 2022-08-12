b471965ce4fde78a33c4519f062ce16670d6e942
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-08-12 14:53:32 +0530

    Time: 5 ms (6.88%), Space: 42.5 MB (70.14%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -27,23 +27,28 @@ class Solution {
         // till queue is empty add elements to a new list according to the flag
         while(!queue.isEmpty()) {
             int level = queue.size();
-            LinkedList<Integer> subList = new LinkedList<>();
+            // LinkedList<Integer> subList = new LinkedList<>();
             // List<Integer> subList = new ArrayList<Integer>(level);
+            int[] subList = new int[level];
             for(int i=0;i<level;i++) {
-                int index=i;
+                // int index=i;
+                int index = flag ? i : level-1-i;
                 
                 if(queue.peek().left !=null) queue.add(queue.peek().left);
                 if(queue.peek().right !=null) queue.add(queue.peek().right);
                 // add to subList
-                if(flag==true) subList.add(queue.poll().val);
-                else subList.addFirst(queue.poll().val);
+                // if(flag==true) subList.add(queue.poll().val);
+                // else subList.addFirst(queue.poll().val);
                 
                 // if(flag==true) subList.add(queue.poll().val);
                 // else subList.add(0, queue.poll().val);
+                
+                subList[index] = queue.poll().val;
             }
             // reset flag and add to wrapper
             flag=!flag;
-            wrapper.add(subList);
+            // wrapper.add(subList);
+            wrapper.add(Arrays.stream(subList).boxed().collect(Collectors.toList()));
         }
         return wrapper;
     }
