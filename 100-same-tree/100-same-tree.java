886a0383a15dfee5586f4ef89ec22fb6878b9cfa
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-08-12 14:29:01 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (67.28%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -17,26 +17,26 @@ class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null || q==null) return p==q;
         
-        // return (p.val==q.val) && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        return (p.val==q.val) && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
         
-        Queue<TreeNode> queue = new LinkedList<>();
-        if(p!=null && q!=null) {
-            queue.add(p);
-            queue.add(q);
-        }
-        while(!queue.isEmpty()) {
-            TreeNode first = queue.poll();
-            TreeNode second = queue.poll();
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         if(p!=null && q!=null) {
+//             queue.add(p);
+//             queue.add(q);
+//         }
+//         while(!queue.isEmpty()) {
+//             TreeNode first = queue.poll();
+//             TreeNode second = queue.poll();
             
-            if(first==null && second==null) continue;
-            if(first==null || second==null) return first==second;
-            if(first.val!=second.val) return false;
+//             if(first==null && second==null) continue;
+//             if(first==null || second==null) return first==second;
+//             if(first.val!=second.val) return false;
                 
-            queue.add(first.left);
-            queue.add(second.left);
-            queue.add(first.right);
-            queue.add(second.right);
-        }
-        return true;
+//             queue.add(first.left);
+//             queue.add(second.left);
+//             queue.add(first.right);
+//             queue.add(second.right);
+//         }
+//         return true;
     }
 }
