d9d9d19576d789e360d9144bcf1d36438618ff1b
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-08-22 15:11:54 +0530

    Time: 1 ms (99.47%), Space: 44.7 MB (98.25%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -4,15 +4,15 @@ class Solution {
         int lo = 0, hi = nums.length - 1;
         while (lo < hi) {
             int sum = nums[lo] + nums[hi];
+            
             if (sum == target) {
+                // while (lo < hi && nums[lo] == nums[lo - 1]) ++lo;
                 return new int[]{lo+1, hi+1};
             } else if (sum < target) {
                 ++lo;
             } else {
                 --hi;
             }
-            // while (lo < hi && nums[lo] == nums[lo - 1])
-            //         ++lo;
         }
         return new int[0];
     }
