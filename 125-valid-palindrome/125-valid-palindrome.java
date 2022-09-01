17fb29746e03856577f23afbc2010bfb73e4d3d3
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-01 14:41:39 +0530

    Time: 4 ms (90.40%), Space: 43 MB (87.85%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+    for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+      while (i < j && !isLetterOrDigit(s.charAt(i))) {
+        i++;
+      }
+      while (i < j && !isLetterOrDigit(s.charAt(j))) {
+        j--;
+      }
+
+      if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+        return false;
+    }
+
+    return true;
+    }
+    
+    public boolean isLetterOrDigit(char ch) {
+        return ((ch >= 'a' && ch<='z') ||
+                (ch >= 'A' && ch<='Z') ||
+                (ch >= '0' && ch<='9'));
+    }
+}
