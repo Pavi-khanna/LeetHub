45f0e1478cba79d0496fcfc9c8e4286922e45b50
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-02 20:18:18 +0530

    Time: 3 ms (40.26%), Space: 44.8 MB (59.06%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,17 +1,50 @@
 class Solution {
     public int maxProduct(int[] A) {
-        // if (A == null || A.length == 0) {
-        //     return 0;
+        
+        // int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE, sum = 1, begin = 0, end = 0, start = 0;
+        // for(int i=0; i<nums.length;i++) {
+        //     sum *= nums[i];
+        //     if(max<sum) {
+        //         max = Math.max(sum, nums[i]);
+        //         start = begin;
+        //         end = i;
+        //     }
+        //     // if(sum<0) {
+        //     //     sum = 1;
+        //     //     begin = i+1;
+        //     // }
         // }
-        int max = A[0], min = A[0], result = A[0], end;
+        // // System.out.println(Arrays.toString(Arrays.copyOfRange(nums, start, end+1)));
+        // return max;
+        
+        
+        // n^2, 1
+//         if (nums.length == 0) return 0;
+//         int result = nums[0];
+//         for (int i = 0; i < nums.length; i++) {
+//             int accu = 1;
+//             for (int j = i; j < nums.length; j++) {
+//                 accu *= nums[j];
+//                 result = Math.max(result, accu);
+//             }
+//         }
+//         return result;
+        
+        
+        // n, 1
+        if (A.length == 0) return 0;
+        int max = A[0], min = A[0], result = A[0], end=0,start=0;
         for (int i = 1; i < A.length; i++) {
             int temp = max;
+            
             max = Math.max(Math.max(max * A[i], min * A[i]), A[i]);
             min = Math.min(Math.min(temp * A[i], min * A[i]), A[i]);
+            
+            
             if (max > result) {
                 result = max;
                 end = i;
-                // System.out.println(A[i]+" "+end+" "+result);
+                // System.out.println(A[i]+" "+start+" "+end+" "+result);
             }
         }
         return result;
