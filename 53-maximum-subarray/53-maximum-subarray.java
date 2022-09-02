827424d3a01526ab936cd9a577dcfda84270166e
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-02 17:50:14 +0530

    Time: 2 ms (74.43%), Space: 73.9 MB (51.82%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -13,18 +13,21 @@ class Solution {
         
         
         // n, 1
+        // agar sb pos h toh toh aage jaane pe max sum hi milega km kaise hoga sum? jb neg milenge elements and jn sum hi neg m chla jae toh usse chati ka bojh bnane ki zarurat nhi h vhin chd do usse and move on
         int max = Integer.MIN_VALUE, sum = 0, begin = 0, end = 0, start = 0;
         for(int i=0; i<nums.length;i++) {
             sum += nums[i];
-            if(max<sum) {
-                max = sum;
-                start = begin;
-                end = i;
-            }
-            if(sum<0) {
-                sum = 0;
-                begin = i+1;
-            }
+            // if(max<sum) {
+            //     max = sum;
+            //     start = begin;
+            //     end = i;
+            // }
+            // if(sum<0) {
+            //     sum = 0;
+            //     begin = i+1;
+            // }
+            max = Math.max(sum, max);
+            if(sum<0) sum = 0;
         }
         // System.out.println(Arrays.toString(Arrays.copyOfRange(nums, start, end+1)));
         return max;
