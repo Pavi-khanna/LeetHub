60a453ec7b81f63de08eb5ec5bcb1325a81c9334
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-06 18:22:16 +0530

    Time: 1 ms (80.01%), Space: 43.7 MB (8.50%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -10,21 +10,42 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode prehead = new ListNode(-1);
+//         ListNode prehead = new ListNode(-1);
         
-        ListNode tail = prehead;
-        while(l1 != null && l2 != null) {
-            if(l1.val <= l2.val) {
-                tail.next = l1;
-                l1 = l1.next;
-            } else {
-                tail.next = l2;
-                l2 = l2.next;
-            }
-            tail = tail.next;
+//         ListNode tail = prehead;
+//         while(l1 != null && l2 != null) {
+//             if(l1.val <= l2.val) {
+//                 tail.next = l1;
+//                 l1 = l1.next;
+//             } else {
+//                 tail.next = l2;
+//                 l2 = l2.next;
+//             }
+//             tail = tail.next;
+//         }
+        
+//         tail.next = l1 == null ? l2 : l1;
+//         return prehead.next;
+        
+        ListNode h = new ListNode(0);
+    ListNode ans=h;
+    while (l1 != null && l2 != null) {
+        if (l1.val < l2.val) {
+            h.next = l1;
+            h = h.next;
+            l1 = l1.next;
+        } else {
+            h.next = l2;
+            h = h.next;
+            l2 = l2.next;
         }
-        
-        tail.next = l1 == null ? l2 : l1;
-        return prehead.next;
+    }
+    if(l1==null){
+        h.next=l2;
+    }
+    if(l2==null){
+        h.next=l1;
+    } 
+    return ans.next;
     }
 }
