9302cc2c37606a22a43481d010e487ef8fe84629
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-07 15:50:40 +0530

    Time: 4 ms (69.71%), Space: 48 MB (41.75%) - LeetHub

---
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        
+        ListNode l = addTwoNumbers1(l1,l2);
+        return reverseList(l);
+    }
+    
+    public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
+        ListNode first = new ListNode(0);
+        ListNode curr = first;
+        int carry = 0;
+        
+        while(l1!=null || l2!=null || carry==1) {
+            int sum = 0;
+            if(l1!=null) {
+                sum+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null) {
+                sum+=l2.val;
+                l2=l2.next;
+            }
+            
+            sum+=carry;
+            carry=sum/10;
+            ListNode node = new ListNode(sum%10);
+            curr.next = node;
+            curr = curr.next;
+        }
+        return first.next;
+    }
+    
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        ListNode temp = head;
+        while(temp!=null) {
+            ListNode next = temp.next;
+            temp.next = newHead;
+            newHead = temp;
+            temp = next;
+        }
+        return newHead;
+    }
+}
