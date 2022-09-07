686875169b55fefcd5702621e1c1f4698bdcd3e6
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-08 00:16:35 +0530

    Time: 9 ms (21.07%), Space: 47.7 MB (68.69%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.java
@@ -10,10 +10,42 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        l1 = reverseList(l1);
-        l2 = reverseList(l2);
+        // n1+n2, 1
+//         l1 = reverseList(l1);
+//         l2 = reverseList(l2);
+//         return reverseList(addTwoNumbers1(l1,l2));
         
-        return reverseList(addTwoNumbers1(l1,l2));
+        
+        if(l1 == null && l2 == null) return null;
+        if(l1 == null || l2 == null) return l1 == null ? l2 : l1;
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+        
+        ListNode curr = l1;
+        while(curr != null){
+            s1.add(curr.val);
+            curr = curr.next;
+        }
+        
+        curr = l2;
+        while(curr != null){
+            s2.add(curr.val);
+            curr = curr.next;
+        }
+        
+        ListNode head = null;
+        int carry = 0;
+        // build result list backwards
+        while(!s1.isEmpty() || !s2.isEmpty() || carry==1){
+            int x = !s1.isEmpty() ? s1.pop() : 0;
+            int y = !s2.isEmpty() ? s2.pop() : 0;
+            int sum = x + y + carry;
+            curr = new ListNode(sum % 10);
+            curr.next = head;
+            head = curr;
+            carry = sum / 10;
+        }
+        return head;
     }
     
     public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
