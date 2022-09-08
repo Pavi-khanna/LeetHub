31af993826b3dd7acc82478a731490d65adbe83c
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-08 17:43:11 +0530

    Time: 7 ms (58.30%), Space: 48.8 MB (50.50%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.java
+++ 518-coin-change-ii/518-coin-change-ii.java
@@ -6,10 +6,14 @@ class Solution {
         
         int n = coins.length;
         int[][] t = new int[n+1][amount+1];
-        for(int i=0; i<n+1; i++) {
-            Arrays.fill(t[i], -1);
-        }
-        int ans = coinChange(t, n, amount, coins);
+        // for(int i=0; i<n+1; i++) {
+        //     Arrays.fill(t[i], -1);
+        // }
+        // int ans = coinChange(t, n, amount, coins);
+        // return ans;
+        
+        
+        int ans = coinChange(t, coins);
         return ans;
     }
     
@@ -44,15 +48,15 @@ class Solution {
     
     static int coinChange(int[][] t, int coins[]) {
          int rows = t.length, cols = t[0].length;
-         for(int j=0; j<cols; j++) t[0][j] = Integer.MAX_VALUE-1;
-         for(int i=0; i<rows; i++) t[i][0] = 0;
+         for(int j=0; j<cols; j++) t[0][j] = 0;
+         for(int i=0; i<rows; i++) t[i][0] = 1;
          
          for(int i=1; i<rows; i++) {
              for(int j=1; j<cols; j++) {
                  if(coins[i-1] > j) {
                     t[i][j] = t[i-1][j];
                  } else {
-                    t[i][j] =   Math.min(1+t[i][j-coins[i-1]] , t[i-1][j]);
+                    t[i][j] = t[i][j-coins[i-1]] + t[i-1][j];
                  }
              }
          }
