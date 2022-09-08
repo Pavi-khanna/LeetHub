76bb5a287def8e3cc5a35eee855d65adfafceded
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-08 13:42:30 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (17.34%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -6,15 +6,24 @@ class Solution {
        // return fib(n-1)+fib(n-2);
         
         
-        // n,1
-        int first=0, second=1, current=0;
-        
+        // n, n
+        int[] cache = new int[n+1];
+        cache[1] = 1;
         for(int i=2; i<=n; i++) {
-            current = first+second;
-            first = second;
-            second = current;
+            cache[i] = cache[i-1]+cache[i-2];
         }
-        return current;
+        return cache[n];
+        
+        
+        // n,1
+//         int first=0, second=1, current=0;
+        
+//         for(int i=2; i<=n; i++) {
+//             current = first+second;
+//             first = second;
+//             second = current;
+//         }
+//         return current;
         
         
         // logn, 1
