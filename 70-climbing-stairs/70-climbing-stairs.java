04d973ae5bbaca010ea354cb91bd14965f920ec4
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-08 15:22:34 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (27.40%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -7,22 +7,22 @@ class Solution {
         
         
         // n, n
-        int[] cache = new int[n+1];
-        cache[1] = 1;
-        cache[2] = 2;
-        for(int i=3; i<=n; i++) {
-            cache[i] = cache[i-1]+cache[i-2];
+        // int[] cache = new int[n+1];
+        // cache[1] = 1;
+        // cache[2] = 2;
+        // for(int i=3; i<=n; i++) {
+        //     cache[i] = cache[i-1]+cache[i-2];
+        // }
+        // return cache[n];
+        
+        
+        int first=1, second=1, current=0;
+        
+        for(int i=2; i<=n; i++) {
+            current = first+second;
+            first = second;
+            second = current;
         }
-        return cache[n];
-        
-        
-//         int first=1, second=1, current=0;
-        
-//         for(int i=2; i<=n; i++) {
-//             current = first+second;
-//             first = second;
-//             second = current;
-//         }
-//         return current;
+        return current;
     }
 }
