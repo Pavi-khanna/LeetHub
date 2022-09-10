cb3f9c7a57673da8debc19dc1e88f19291841f5c
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-10 17:37:39 +0530

    Time: 126 ms (17.24%), Space: 105.9 MB (11.05%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -21,37 +21,37 @@ class Solution {
         
         
         
-//         if (intervals.length == 0) return 0;
-//         Arrays.sort(intervals, (i1, i2) -> i1[1] - i2[1]);
-//         int end = intervals[0][1];
-        
-//         int count = 0;
-//         for (int i = 1; i < intervals.length; i++) {
-//             if (intervals[i][0] < end) {
-//                 count++;
-//             } else {
-//                 end = intervals[i][1];
-//             }
-//         }
-//         return count;
-        
-        
-        List<int[]> res = new ArrayList<>();
-        if(intervals.length == 0 || intervals == null) {return 0;}
-        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
-        int start=intervals[0][0];
+        if (intervals.length == 0) return 0;
+        Arrays.sort(intervals, (i1, i2) -> i1[1] - i2[1]);
         int end = intervals[0][1];
         
-        int count=0;
-        for(int i=1; i<intervals.length; i++) {
-            if(end>intervals[i][0]) { end = Math.min(end,intervals[i][1]); count++; }       
-            // 1,3 & 2,4
-            else {
-                res.add(new int[]{start,end});                // 1,3 & 5,6
+        int count = 0;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < end) {
+                count++;
+            } else {
                 end = intervals[i][1];
             }
         }
-        res.add(new int[]{start,end});                        // last one
         return count;
+        
+        
+//         List<int[]> res = new ArrayList<>();
+//         if(intervals.length == 0 || intervals == null) {return 0;}
+//         Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+//         int start=intervals[0][0];
+//         int end = intervals[0][1];
+        
+//         int count=0;
+//         for(int i=1; i<intervals.length; i++) {
+//             if(end>intervals[i][0]) { end = Math.min(end,intervals[i][1]); count++; }       
+//             // 1,3 & 2,4
+//             else {
+//                 res.add(new int[]{start,end});                // 1,3 & 5,6
+//                 end = intervals[i][1];
+//             }
+//         }
+//         res.add(new int[]{start,end});                        // last one
+//         return count;
     }
 }
