8a607145b747fcb9a44546a3443e4e81125a427f
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-12 02:53:24 +0530

    Time: 8 ms (28.98%), Space: 43.6 MB (79.45%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.java
+++ 137-single-number-ii/137-single-number-ii.java
@@ -19,16 +19,27 @@ class Solution {
         // return 0;
         
         
+        // long setSum = 0, arraySum = 0;
+        // Set<Integer> set = new HashSet<>();
+        // for(int num : nums) {
+        //     if(!set.contains(num)) {
+        //         set.add(num);
+        //         setSum += num;
+        //     }
+        //     arraySum += num;
+        // }
+        // return (int)((3*(setSum) - arraySum) /2);
         
-        long setSum = 0, arraySum = 0;
-        Set<Integer> set = new HashSet<>();
-        for(int num : nums) {
-            if(!set.contains(num)) {
-                set.add(num);
-                setSum += num;
+        
+        int res = 0;
+        for(int i = 0; i < 32; i++) {
+            int cur = 0;
+            for(int num: nums) {
+                cur+= (num >> i) & 1;
             }
-            arraySum += num;
+            cur %= 3;
+            res = res | (cur << i);
         }
-        return (int)((3*(setSum) - arraySum) /2);
+        return res;
     }
 }
