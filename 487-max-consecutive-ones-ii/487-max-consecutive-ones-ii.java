b8bf3c97adbe032eebece48f5fa127b89acfd352
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-11 23:31:24 +0530

    Time: 8 ms (11.48%), Space: 56.8 MB (53.29%) - LeetHub

--- 487-max-consecutive-ones-ii/487-max-consecutive-ones-ii.java
+++ 487-max-consecutive-ones-ii/487-max-consecutive-ones-ii.java
@@ -1,26 +1,45 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         // n^2, 1
-        int longestSequence = 0;
-        for (int left = 0; left < nums.length; left++) {
-            int numZeroes = 0;
-            // check every consecutive sequence
-            for (int right = left; right < nums.length; right++) {
-                // count how many 0's
-                if (nums[right] == 0) {
-                    numZeroes += 1;
-                }
-                if(numZeroes>1) break;
-                // # update answer if it's valid
-                // if it has only 1s or just one 0
-                if (numZeroes <= 1) {
-                    longestSequence = Math.max(longestSequence, right - left + 1);
-                }
+        // int longestSequence = 0;
+        // for (int left = 0; left < nums.length; left++) {
+        //     int numZeroes = 0;
+        //     // check every consecutive sequence
+        //     for (int right = left; right < nums.length; right++) {
+        //         // count how many 0's
+        //         if (nums[right] == 0) {
+        //             numZeroes += 1;
+        //         }
+        //         if(numZeroes>1) break; // pruning
+        //         // # update answer if it's valid
+        //         // if it has only 1s or just one 0
+        //         if (numZeroes <= 1) {
+        //             longestSequence = Math.max(longestSequence, right - left + 1);
+        //         }
+        //     }
+        // }
+        // return longestSequence;
+        
+        
+        
+        int i = 0, j = 0;
+        int numZeroes = 0, max = 0;
+        
+        if(nums.length<1) return -1;
+        
+        while(j < nums.length) {
+            if(nums[j]==0) numZeroes++;
+            
+            if(numZeroes > 1) {
+                // max = Math.max(max, sum);
+                // sum -= arr.get(i);
+                if(nums[i]==0) numZeroes--;
+                i++;
+            } else if(numZeroes <=1) {
+                max = Math.max(max, j-i+1);
             }
+            j++;
         }
-        return longestSequence;
-        
-        
-        
+        return max;
     }
 }
