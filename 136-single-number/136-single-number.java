c38298a00347ec48a98d10559f988e1d67416125
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-12 01:26:02 +0530

    Time: 18 ms (24.22%), Space: 52.6 MB (26.03%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -10,17 +10,31 @@ class Solution {
         // return nums[nums.length-1];
         
         
-        Map<Integer, Integer> map = new HashMap<>();
+        // n, n
+        // Map<Integer, Integer> map = new HashMap<>();
+        // for(int num : nums) {
+        //     map.put(num, map.getOrDefault(num,0)+1);
+        // }
+        // for(int num : nums) {
+        //     if(map.get(num) == 1) return num;
+        // }
+        // return 0;
         
+        
+        // n, n
+        int setSum = 0, arraySum = 0;
+        Set<Integer> set = new HashSet<>();
         for(int num : nums) {
-            map.put(num, map.getOrDefault(num,0)+1);
+            if(!set.contains(num)) {
+                set.add(num);
+                setSum += num;
+            }
+            arraySum += num;
         }
-        for(int num : nums) {
-            if(map.get(num) == 1) return num;
-        }
-        return 0;
+        return 2*(setSum) - arraySum;
         
         
+        // n, 1
         // int ans = nums[0];
         // for(int i=1; i<nums.length; i++) {
         //     ans ^= nums[i];
