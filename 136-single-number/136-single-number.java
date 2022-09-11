6c48dfd9a8f9e2a488523770c6e965eaa5782982
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-12 01:21:14 +0530

    Time: 10 ms (35.86%), Space: 42.9 MB (87.44%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,13 +1,24 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int num = 0;
-        for(int i=0; i<nums.length-1; i++) {
-            if(nums[i]==nums[i+1]) i++;
-            else return nums[i];
-        }
-        return nums[nums.length-1];
+        // nlogn, 1
+        // Arrays.sort(nums);
+        // int num = 0;
+        // for(int i=0; i<nums.length-1; i++) {
+        //     if(nums[i]==nums[i+1]) i++;
+        //     else return nums[i];
+        // }
+        // return nums[nums.length-1];
         
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+        for(int num : nums) {
+            if(map.get(num) == 1) return num;
+        }
+        return 0;
         
         
         // int ans = nums[0];
