6f530ebf69e9f10bd2a78754f26a60fa6bf86c4f
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-11 23:52:03 +0530

    Time: 7 ms (17.11%), Space: 54.2 MB (79.67%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,56 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+//         int snowBallSize = 0;
+        
+//         for(int i=0; i<nums.length; i++) { // 0 1 2 3 4
+//             if(nums[i]<0) {
+//                 // System.out.println(i+"a");
+//                 snowBallSize++; // 1 2
+//             } else if(snowBallSize > 0) {
+//                 // System.out.println(i+"b"+snowBallSize);
+//                 int temp = nums[i-snowBallSize];
+//                 nums[i-snowBallSize] = nums[i];
+//                 nums[i] = temp;
+//             }
+//             System.out.println(Arrays.toString(nums) +" "+ i+": "+ (i-snowBallSize)+" "+nums[i]);
+//         }
+        
+        
+        
+        
+        List<Integer> list = new ArrayList<>();
+        for(int num : nums) {
+            if(num!=0){
+                list.add(num);
+            }
+        }
+        int zeroes = nums.length-list.size();
+        while(zeroes > 0) {
+            list.add(0);
+            zeroes--;
+        }
+        for(int i=0; i<nums.length; i++) {
+            nums[i] = list.get(i);
+        }
+        
+        
+        
+//         int n = nums.length;
+//         if(n==0 || n==1)  return;
+        
+//         int left = 0, right = 0;
+        
+//         while(right<n) {
+//             if(nums[right] == 0) ++right;
+//             else {
+//                 int temp = nums[left];
+//                 nums[left++] = nums[right];
+//                 nums[right++] = temp;
+//             }
+//         }
+    }
+}
+// 0 1 0 3 12
+// 1 0 0 3 12  o O 
+// 1 3 0 0 12
+// 1 3 12 0 0
