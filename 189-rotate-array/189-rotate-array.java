2a4448e30ae3f98755d218b51297acf335798473
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-12 23:10:38 +0530

    Time: 16 ms (5.00%), Space: 64.2 MB (63.66%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -15,10 +15,29 @@ class Solution {
         
         
         // n, 1
-        k%=n;
-        reverse(nums, 0, n-1);
-        reverse(nums, 0, k-1);
-        reverse(nums, k, n-1);
+        // k%=n;
+        // reverse(nums, 0, n-1);
+        // reverse(nums, 0, k-1);
+        // reverse(nums, k, n-1);
+        
+        
+        k %= n;
+        k = n-k;
+        
+        for(int i=0; i<gcd(k, n); i++) {
+            int temp = nums[i], j = i;
+            
+            while (true) {
+				int d = j + k;
+                if (d >= n)
+                    d = d - n;
+                if (d == i)
+                    break;
+                nums[j] = nums[d];
+                j = d;
+			}
+			nums[j] = temp;
+        }
     }
     
     public void reverse(int[] a, int start, int end) {
@@ -27,6 +46,11 @@ class Solution {
             a[start++] = a[end];
             a[end--] = num;
         }
+    }
+    
+    public int gcd(int a, int b) {
+        if (b == 0) return a;
+        else return gcd(b, a % b);
     }
     
     public void rotateByOne(int[] a) {
