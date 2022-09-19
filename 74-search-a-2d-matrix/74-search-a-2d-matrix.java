7c7983d15386d4abfd43665e64b61254d13d9196
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-19 19:41:36 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (60.61%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -9,45 +9,44 @@ class Solution {
         // return false;
         
         
-        // n, 1
-//         int n = matrix.length, m = matrix[0].length; 
-//         int i=0, j=m-1;
-//         while(i<n && j>=0) {
-//             if(matrix[i][j]==t) return true;
-            
-//             if(matrix[i][j]>t) j--;
-//             else i++;
-//         }
-//         return false;
-        
-        
-        // log(mn), 1
-        // int n = matrix.length; 
-        // int m = matrix[0].length; 
-        // int lo = 0, hi = n*m-1;
-        // while(lo <= hi) {
-        //     int mid = (lo+(hi-lo)/2);
-        //     if(matrix[mid/m][mid%m] == t){
-        //         return true;
-        //     }
-        //     if(matrix[mid/m][mid%m] < t) {
-        //         lo = mid+1;
-        //     }
-        //     else{
-        //         hi = mid-1;
-        //     }
+        // n+m, 1
+        // int n = matrix.length, m = matrix[0].length; 
+        // int i=0, j=m-1;
+        // while(i<n && j>=0) {
+        //     if(matrix[i][j]==t) return true;
+        //     if(matrix[i][j]>t) j--;// down
+        //     else i++;// left
         // }
         // return false;
         
         
-        int mid, low =0, high=matrix.length-1;
-        while(low<=high) {
-            mid = (low +(high-low)/2);
-            if(t>=matrix[mid][0] && t<=matrix[mid][matrix[0].length-1]) return search(t, matrix[mid], 0, matrix[0].length-1);
-            else if(matrix[mid][0]<t) low = mid+1;
-            else high = mid-1;
+        // log(mn), 1
+        int n = matrix.length; 
+        int m = matrix[0].length; 
+        int lo = 0, hi = n*m-1;
+        while(lo <= hi) {
+            int mid = (lo+(hi-lo)/2);
+            if(matrix[mid/m][mid%m] == t){
+                return true;
+            } else if(matrix[mid/m][mid%m] < t) {
+                lo = mid+1;
+            } else{
+                hi = mid-1;
+            }
         }
         return false;
+        
+        
+        // logm+logn, 1
+        // int mid, low =0, high=matrix.length-1;
+        // while(low<=high) {
+        //     mid = (low +(high-low)/2);
+        //     if(t>=matrix[mid][0] && t<=matrix[mid][matrix[0].length-1]) 
+        //         return search(t, matrix[mid], 0, matrix[0].length-1);
+        //     else if(matrix[mid][0]<t) low = mid+1;
+        //     else high = mid-1;
+        // }
+        // return false;
     }
     
     public boolean search(int target, int[] arr, int low, int high) {
