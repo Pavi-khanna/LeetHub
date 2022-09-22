ca09adf06d7cdafc29c23141a70cb967baecf636
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-22 15:03:59 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (45.11%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -18,13 +18,40 @@ class Solution {
         // n^2, 1
         int n = matrix.length;
 
-        for (int i = 0; i < (n + 1) / 2; i ++) {
+        // for (int i = 0; i < (n + 1) / 2; i ++) {
+        //     for (int j = 0; j < n / 2; j++) {
+        //         int temp = matrix[n - 1 - j][i];
+        //         matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+        //         matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+        //         matrix[j][n - 1 - i] = matrix[i][j];
+        //         matrix[i][j] = temp;
+        //     }
+        // }
+        
+        
+        // n^2, 1
+        transpose(matrix);
+        reverse(matrix);
+    }
+    
+    public void transpose(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+    }
+    
+    public void reverse(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < n / 2; j++) {
-                int temp = matrix[n - 1 - j][i];
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
-                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
-                matrix[j][n - 1 - i] = matrix[i][j];
-                matrix[i][j] = temp;
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
             }
         }
     }
