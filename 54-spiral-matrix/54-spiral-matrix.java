f6450dc74893b4ff2ee68fc569f9e888e3f78687
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-22 15:32:34 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (79.28%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -3,53 +3,46 @@ class Solution {
         List<Integer> result = new ArrayList<>();
         int up=0, left=0, right = matrix[0].length-1,down = matrix.length-1; 
         
-//         while(left<=right && up<=down) {
-//             // top row
-//             for(int col = left; col <= right; col++) {
-//                 result.add(matrix[up][col]);
-//             }
-            
-//             // right col
-//             for(int row = up + 1; row <= down; row++) {
-//                 result.add(matrix[row][right]);
-//             }
-            
-//             //if(!(left<right && up<down)) {break;}
-            
-//             // bottom row
-//             if(up<down) {
-//             for (int col = right - 1; col >= left; col--) {
-//                 result.add(matrix[down][col]);
-//                 }    
-//             }
-            
-//             // left col
-//             if(left<right) {
-//             for (int row = down - 1; row > up; row--) {
-//                 result.add(matrix[row][left]);
-//                 }
-//             }
-            
-//             left++;
-//             right--;
-//             up++;
-//             down--;
-//         }
+        while(left<=right && up<=down) {
+            // top row
+            for(int col = left; col <= right; col++) result.add(matrix[up][col]);
         
-        int n = matrix.length, m = matrix[0].length, i=0, j=-1, dir=1;
-        while(n*m > 0) {
-            for(int p=0; p<m; p++) {
-                j+=dir;
-                result.add(matrix[i][j]);
-            }
-            n-=1;
-            for(int q=0; q<n; q++) {
-                i+=dir;
-                result.add(matrix[i][j]);
-            }
-            m-=1;
-            dir *= -1;
+            // right col
+            for(int row = up + 1; row <= down; row++) result.add(matrix[row][right]);
+            if(!(left<right && up<down)) {break;}
+            
+            // bottom row
+            // if(up<down) {
+            for (int col = right - 1; col >= left; col--) result.add(matrix[down][col]);   
+            // }
+            // left col
+            // if(left<right) {
+            for (int row = down - 1; row > up; row--) result.add(matrix[row][left]);
+            // }
+            
+            left++;
+            right--;
+            up++;
+            down--;
         }
+        
+        // _
+        //  |  |_
+        
+        // int n = matrix.length, m = matrix[0].length, i=0, j=-1, dir=1;
+        // while(n*m > 0) {
+        //     for(int p=0; p<m; p++) {
+        //         j+=dir;
+        //         result.add(matrix[i][j]);
+        //     }
+        //     n-=1;
+        //     for(int q=0; q<n; q++) {
+        //         i+=dir;
+        //         result.add(matrix[i][j]);
+        //     }
+        //     m-=1;
+        //     dir *= -1;
+        // }
         return result;
     }
 }
