e7260f5ffa70670ace04440c48798553fc3088fc
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-09-28 17:32:11 +0530

    Time: 6 ms (12.79%), Space: 42.3 MB (64.74%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.java
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = -1;
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i]>=k) continue;
+            // if(nums[i]>=k) continue;
                 int j = search(nums, i+1, nums.length-1, k-nums[i]);
                 if(j != -1) {
                     int sum = nums[i] + nums[j];
