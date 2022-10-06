b599810c356d5360dbfa8f5af5855eebef7bae30
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-06 15:56:00 +0530

    Time: 1 ms (83.68%), Space: 51.6 MB (21.43%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -43,9 +43,9 @@ class Solution {
         
         
         int xor1 = 0, xor2 = 0;
-        for(int i=1; i<=nums.length; i++) {
-            xor1 ^= i;
-            xor2 ^= nums[i-1];
+        for(int i=0; i<nums.length; i++) {
+            xor1 ^= (i+1);
+            xor2 ^= nums[i];
         }
         return xor1^xor2;
     }
