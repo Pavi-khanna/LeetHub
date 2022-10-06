dd68e94d434b0709a6ad980afb098117e2c8e3ed
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-06 15:24:25 +0530

    Time: 586 ms (5.01%), Space: 44 MB (84.47%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.java
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -1,17 +1,36 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        if(intervals.length == 0 || intervals == null) {return true;}
-        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
-        int start=intervals[0][0];
-        int end = intervals[0][1];
-        
-        int count=0;
-        for(int i=1; i<intervals.length; i++) {
-            if(end>intervals[i][0]) { end = Math.min(end,intervals[i][1]); return false; }       
-            else {
-                end = intervals[i][1];
+        // n^2, 1
+        for (int i = 0; i < intervals.length; i++) {
+            for (int j = i + 1; j < intervals.length; j++) {
+                if (overlap(intervals[i], intervals[j])) {
+                    return false;
+                }
             }
         }
         return true;
+        
+        
+//         if(intervals.length == 0 || intervals == null) {return true;}
+//         Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+//         int start=intervals[0][0];
+//         int end = intervals[0][1];
+        
+//         int count=0;
+//         for(int i=1; i<intervals.length; i++) {
+//             if(end>intervals[i][0]) { end = Math.min(end,intervals[i][1]); return false; }       
+//             else {
+//                 end = intervals[i][1];
+//             }
+//         }
+//         return true;
+        // ___       ____    __    _____
+        //  ____   ____     ____    __
+    }
+    
+    private boolean overlap(int[] interval1, int[] interval2) {
+        int start1 = interval1[0], start2 = interval2[0], end1 = interval1[1], end2 = interval2[1];
+        return ((start2<=start1 && start1<end2) 
+            || (start1<=start2 && start2<end1));
     }
 }
