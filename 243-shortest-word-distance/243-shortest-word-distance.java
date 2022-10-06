4709182e1c4e7477be0f4d5dc8210308ca4315dc
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-06 13:00:04 +0530

    Time: 3 ms (60.05%), Space: 44.5 MB (57.35%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.java
+++ 243-shortest-word-distance/243-shortest-word-distance.java
@@ -1,31 +1,31 @@
 class Solution {
     public int shortestDistance(String[] wordsDict, String word1, String word2) {
-        int minDistance = wordsDict.length;
-        for (int i = 0; i < wordsDict.length; i++) {
-            if (wordsDict[i].equals(word1)) {
-                for (int j = 0; j < wordsDict.length; j++) {
-                    if (wordsDict[j].equals(word2)) {
-                        minDistance = Math.min(minDistance, Math.abs(i - j));
-                    }
-                }
-            }
-        }
-        return minDistance;
+        // int minDistance = wordsDict.length;
+        // for (int i = 0; i < wordsDict.length; i++) {
+        //     if (wordsDict[i].equals(word1)) {
+        //         for (int j = 0; j < wordsDict.length; j++) {
+        //             if (wordsDict[j].equals(word2)) {
+        //                 minDistance = Math.min(minDistance, Math.abs(i - j));
+        //             }
+        //         }
+        //     }
+        // }
+        // return minDistance;
         
         
         
         // n*m, 1 (m= len of w1 & w2)
-//         int first=-1, second=-1, distance=wordsDict.length;
-//         for(int i=0; i<wordsDict.length; i++) {
-//             // if(distance==1) return distance;
+        int first=-1, second=-1, distance=wordsDict.length;
+        for(int i=0; i<wordsDict.length; i++) {
+            // if(distance==1) return distance;
             
-//             String word = wordsDict[i];
-//             if(word.equals(word1)) first=i;
-//             else if(word.equals(word2)) second=i;
-//             int currDistance = second-first;
+            String word = wordsDict[i];
+            if(word.equals(word1)) first=i;
+            else if(word.equals(word2)) second=i;
+            int currDistance = second-first;
             
-//             if(first!=-1 && second!=-1) distance = Math.min(distance, Math.abs(currDistance));
-//         }
-//         return distance;
+            if(first!=-1 && second!=-1) distance = Math.min(distance, Math.abs(currDistance));
+        }
+        return distance;
     }
 }
