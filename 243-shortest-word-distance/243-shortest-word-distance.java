1a7d59c673e619883ee3657737033d4a638e45f2
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-06 12:38:26 +0530

    Time: 1 ms (100.00%), Space: 42.4 MB (92.22%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.java
+++ 243-shortest-word-distance/243-shortest-word-distance.java
@@ -3,7 +3,7 @@ class Solution {
         // n*m, 1 (m= len of w1 & w2)
         int first=-1, second=-1, distance=wordsDict.length;
         for(int i=0; i<wordsDict.length; i++) {
-            if(distance==1) return distance;
+            // if(distance==1) return distance;
             
             String word = wordsDict[i];
             if(word.equals(word1)) first=i;
