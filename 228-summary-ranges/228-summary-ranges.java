71dae076ef618fd6a5aa3f6fbf9f584c6971bf94
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-06 12:05:19 +0530

    Time: 6 ms (87.10%), Space: 40.8 MB (86.70%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.java
+++ 228-summary-ranges/228-summary-ranges.java
@@ -1,12 +1,31 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        List<String> summary = new ArrayList<>();
-        for(int i=0, j=0; j<nums.length; j++) {
-            if(j+1<nums.length && nums[j+1]==nums[j]+1) continue;
-            else if(i==j) summary.add(nums[i]+"");
-            else summary.add(nums[i]+"->"+nums[j]);
-            i=j+1;
+        // n, 1
+        // List<String> summary = new ArrayList<>();
+        // for(int i=0, j=0; j<nums.length; j++) {
+        //     if(j+1<nums.length && nums[j+1]==nums[j]+1) continue;
+        //     else if(i==j) summary.add(nums[i]+"");
+        //     else summary.add(nums[i]+"->"+nums[j]);
+        //     i=j+1;
+        // }
+        // return summary;
+        
+        
+        List<String> res = new ArrayList<>();
+        if(nums.length == 0) return res;
+        int l=0, r;
+        for(r=1; r<nums.length; r++) {
+            if(nums[r]!=nums[r-1]+1) {
+                res.add(range(nums[l], nums[r-1]));
+                l=r;
+            }
         }
-        return summary;
+        res.add(range(nums[l], nums[r-1]));
+        return res;
+    }
+    
+    private String range(int l, int r) {
+        if(l == r) return ""+l;
+        else return l + "->" + r;
     }
 }
