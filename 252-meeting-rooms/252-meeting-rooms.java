092b448368b8fa0f70a68b76d098acb79ef66d06
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-06 15:39:02 +0530

    Time: 15 ms (25.50%), Space: 44.3 MB (82.62%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.java
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -11,15 +11,12 @@ class Solution {
         // return true;
         
         
-        if(intervals.length == 0 || intervals == null) { return true; }
+        // nlogn, 1
+        // if(intervals.length == 0 || intervals == null) { return true; }
         Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
-        int start=intervals[0][0];
-        int end = intervals[0][1];
         
-        int count=0;
-        for(int i=1; i<intervals.length; i++) {
-            if(end>intervals[i][0]) { end = Math.min(end,intervals[i][1]); return false; }       
-            else end = intervals[i][1];
+        for(int i=0; i<intervals.length-1; i++) {
+            if(intervals[i][1] > intervals[i+1][0]) return false;
         }
         return true;
     }
