f1538731e9088cf66dcc1cb5209a23a67f07cb26
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-07 15:26:40 +0530

    Time: 7 ms (34.14%), Space: 43.7 MB (71.72%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
@@ -27,26 +27,41 @@ class Solution {
 //         return op;
         
         
+        // Set<Integer> set = new HashSet<>();
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
+        // int i = 0;
+        // int j = 0;
+        // while (i < nums1.length && j < nums2.length) {
+        //     if (nums1[i] < nums2[j]) {
+        //         i++;
+        //     } else if (nums1[i] > nums2[j]) {
+        //         j++;
+        //     } else {
+        //         set.add(nums1[i]);
+        //         i++;
+        //         j++;
+        //     }
+        // }
+        // int[] result = new int[set.size()];
+        // int k = 0;
+        // for (Integer num : set) {
+        //     result[k++] = num;
+        // }
+        // return result;
+        
+        
         Set<Integer> set = new HashSet<>();
-        Arrays.sort(nums1);
         Arrays.sort(nums2);
-        int i = 0;
-        int j = 0;
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] < nums2[j]) {
-                i++;
-            } else if (nums1[i] > nums2[j]) {
-                j++;
-            } else {
-                set.add(nums1[i]);
-                i++;
-                j++;
+        for (Integer num : nums1) {
+            if (binarySearch(nums2, num)) {
+                set.add(num);
             }
         }
+        int i = 0;
         int[] result = new int[set.size()];
-        int k = 0;
         for (Integer num : set) {
-            result[k++] = num;
+            result[i++] = num;
         }
         return result;
     }
@@ -58,5 +73,22 @@ class Solution {
             if(set2.contains(s)) op[idx++] = s;
         
         return Arrays.copyOf(op, idx);
+    }
+    
+    public boolean binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
     }
 }
