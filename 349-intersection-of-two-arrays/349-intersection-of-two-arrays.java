af10bdfd31b4fab8af7ffa7a77d66957e6b5a1f4
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-07 15:22:44 +0530

    Time: 6 ms (46.81%), Space: 43.7 MB (66.60%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
@@ -1,13 +1,31 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> intersection = new HashSet<>();
-        for(int i=0; i<nums1.length; i++) {
-            for(int j=0; j<nums2.length; j++) {
-                if(nums1[i]==nums2[j]) intersection.add(nums1[i]);
-            }
-        }
-        return intersection.stream()
-                           .mapToInt(Integer::intValue)
-                           .toArray();
+        // Set<Integer> intersection = new HashSet<>();
+        // for(int i=0; i<nums1.length; i++) {
+        //     for(int j=0; j<nums2.length; j++) {
+        //         if(nums1[i]==nums2[j]) intersection.add(nums1[i]);
+        //     }
+        // }
+        // return intersection.stream()
+        //                    .mapToInt(Integer::intValue)
+        //                    .toArray();
+        
+        
+        Set<Integer> set1 = new HashSet<>();
+        for(int num : nums1) set1.add(num);
+        Set<Integer> set2 = new HashSet<>();
+        for(int num : nums2) set2.add(num);
+        
+        if(set1.size()<set2.size()) return setInt(set1, set2);
+        else return setInt(set2, set1);
+    }
+    
+    public int[] setInt(Set<Integer> set1, Set<Integer> set2) {
+        int[] op = new int[set1.size()];
+        int idx = 0;
+        for(int s : set1)
+            if(set2.contains(s)) op[idx++] = s;
+        
+        return Arrays.copyOf(op, idx);
     }
 }
