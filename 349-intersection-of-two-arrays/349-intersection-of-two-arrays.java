cf7192631d70a7c41a57421b004dc3ae43ea7baf
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-07 15:25:38 +0530

    Time: 9 ms (21.19%), Space: 43.5 MB (78.92%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
@@ -11,20 +11,44 @@ class Solution {
         //                    .toArray();
         
         
-        Set<Integer> set1 = new HashSet<>();
-        for(int num : nums1) set1.add(num);
-        Set<Integer> set2 = new HashSet<>();
-        for(int num : nums2) set2.add(num);
+//         Set<Integer> set1 = new HashSet<>();
+//         for(int num : nums1) set1.add(num);
+//         Set<Integer> set2 = new HashSet<>();
+//         for(int num : nums2) set2.add(num);
         
-        // if(set1.size()<set2.size()) return setInt(set1, set2);
-        // else return setInt(set2, set1);
+//         // if(set1.size()<set2.size()) return setInt(set1, set2);
+//         // else return setInt(set2, set1);
         
-        set1.retainAll(set2);
+//         set1.retainAll(set2);
         
-        int[] op = new int[set1.size()];
-        int idx=0;
-        for(int s:set1) op[idx++] = s;
-        return op;
+//         int[] op = new int[set1.size()];
+//         int idx=0;
+//         for(int s:set1) op[idx++] = s;
+//         return op;
+        
+        
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] result = new int[set.size()];
+        int k = 0;
+        for (Integer num : set) {
+            result[k++] = num;
+        }
+        return result;
     }
     
     public int[] setInt(Set<Integer> set1, Set<Integer> set2) {
