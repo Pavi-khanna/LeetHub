258b8c532e51a80316ec80426360670043f8c0d3
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-08 13:05:23 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (88.73%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -11,42 +11,42 @@
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         // 2n, 1
-        int length = 0;
-        ListNode curr = head;
+//         int length = 0;
+//         ListNode curr = head;
         
-        while(curr != null) {
-            length++;
-            curr = curr.next;
-        }
+//         while(curr != null) {
+//             length++;
+//             curr = curr.next;
+//         }
+//         // first node edge case
+//         if(length==n) {
+//             return head.next;
+//         }
         
-        if(length==n) {
-            return head.next;
-        }
-        
-        int nodeBeforeIndex = length-n-1;
-        curr = head;
-        for(int i=0; i<nodeBeforeIndex; i++) {
-            curr = curr.next;
-        }
-        curr.next = curr.next.next;
-        return head;
+//         int nodeBeforeIndex = length-n-1;
+//         curr = head;
+//         for(int i=0; i<nodeBeforeIndex; i++) {
+//             curr = curr.next;
+//         }
+//         curr.next = curr.next.next;
+//         return head;
         
         
         // n, 1
-//         ListNode first = new ListNode();
-//         first.next = head;
-//         ListNode slow = first, fast = first;
+        ListNode slow = head, fast = head;
         
-//         // move fast n distance
-//         for(int i=0; i<n; i++) { fast = fast.next; }
+        // move fast n distance
+        for(int i=0; i<n; i++) { fast = fast.next; }
         
-//         // move fast & slow - (len-n) distance
-//         // distances - fast = (len-n)+n, slow = len-n
-//         while(fast.next != null) {  // on the last node
-//             fast = fast.next;
-//             slow = slow.next;
-//         }
-//         slow.next = slow.next.next;
-//         return first.next;
+        if(fast == null) return head.next;
+        
+        // move fast & slow - (len-n) distance
+        // distances - fast = (len-n)+n, slow = len-n
+        while(fast.next != null) {  // on the last node - to stop before the one to be removed
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
     }
 }
