55237f9318261580fd0ae6e5e5c9b445883de25b
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-08 22:39:21 +0530

    Time: 3 ms (82.28%), Space: 42.2 MB (59.22%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -1,37 +1,37 @@
 class Solution {
     public int longestPalindrome(String s) {
-        Map<Character, Integer> freq = new HashMap<>();
+        // Map<Character, Integer> freq = new HashMap<>();
+        // boolean oddChar = false;
+        // for(int i=0; i<s.length(); i++) {
+        //     char element = s.charAt(i);
+        //     freq.put(element, freq.getOrDefault(element, 0)+1);
+        // }
+        // // System.out.println(freq);
+        // int length = 0, maxOdd = 0;
+        // for(Map.Entry<Character,Integer> entry : freq.entrySet()) {
+        //     if(entry.getValue()%2 == 0) {
+        //         length += entry.getValue();
+        //     } else {
+        //         length += (entry.getValue()-1);
+        //         oddChar = true;
+        //     }
+        //     // System.out.println(entry.getValue() + " "  + length);
+        // }
+        // return oddChar ? length+1 : length;
+        
+        
+        int[] count = new int[128];
+        for(char c : s.toCharArray()) count[c]++;
         boolean oddChar = false;
-        for(int i=0; i<s.length(); i++) {
-            char element = s.charAt(i);
-            freq.put(element, freq.getOrDefault(element, 0)+1);
-        }
-        // System.out.println(freq);
-        int length = 0, maxOdd = 0;
-        for(Map.Entry<Character,Integer> entry : freq.entrySet()) {
-            if(entry.getValue()%2 == 0) {
-                length += entry.getValue();
-            } else {
-                length += (entry.getValue()-1);
+        
+        int ans = 0;
+        for(int freq : count) {
+            if(freq%2 == 0) ans += freq;
+            else {
+                ans += (freq - 1);
                 oddChar = true;
             }
-            // System.out.println(entry.getValue() + " "  + length);
         }
-        return oddChar ? length+1 : length;
-        
-        
-//         int[] count = new int[128];
-//         for(char c : s.toCharArray()) count[c]++;
-//         boolean oddChar = false;
-        
-//         int ans = 0;
-//         for(int freq : count) {
-//             if(freq%2 == 0) ans += freq;
-//             else {
-//                 ans += (freq - 1);
-//                 oddChar = true;
-//             }
-//         }
-        
+        return oddChar ? ans+1 : ans;
     }
 }
