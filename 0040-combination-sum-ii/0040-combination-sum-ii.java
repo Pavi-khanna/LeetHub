3bb88468cf70cb1ffa6f1dc1375b29a543c4d06c
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-10 21:34:08 +0530

    Time: 5 ms (82.53%), Space: 43.9 MB (51.80%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,70 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        // 2^n*klogn
+        // Arrays.sort(candidates);
+        // Set<List<Integer>> set = findComb(0, candidates, target, new HashSet<>(), new ArrayList<>());
+        // return new ArrayList<>(set);
+        
+        // 2^n*k, k*x
+        Arrays.sort(candidates);
+        return findComb(0, candidates, target, new ArrayList<>(), new ArrayList<>());
+        
+        // return sum(candidates, candidates.length, target, 0, 0, new ArrayList<>(), new ArrayList<>());
+    }
+    
+    private Set<List<Integer>> findComb(int index, int[] c, int target, HashSet<List<Integer>> ans, List<Integer> ds) {
+        if(index == c.length) {
+            if(target==0) {
+                ans.add(new ArrayList<>(ds));
+            }
+            return ans;
+        }
+        
+        if(c[index] <= target) {
+            ds.add(c[index]);
+            findComb(index+1, c, target-c[index], ans, ds);
+            ds.remove(ds.size()-1);
+        }
+        findComb(index+1, c, target, ans, ds);
+        
+        return ans;
+    }
+    
+    private List<List<Integer>> findComb(int index, int[] c, int target, List<List<Integer>> ans, List<Integer> ds) {
+        if(target == 0) {
+            ans.add(new ArrayList<>(ds));
+            return ans;
+        }
+        
+        for(int i=index;i<c.length;i++) {
+            if(i!=index && c[i]==c[i-1]) continue;   // 1st index ko ignore na krde cz i==index
+            if(c[i]>target) break;     // anything on the right of the sorted arr is >
+            
+            ds.add(c[i]);
+            findComb(i+1, c, target-c[i], ans, ds);
+            ds.remove(ds.size()-1);
+        }
+        return ans;
+    }
+    
+    public List<List<Integer>> sum(int arr[], int n, int sum, int i, int s, List<Integer> ds, List<List<Integer>> ans) {
+        if(i == n) {
+           if(s == sum) {
+               // System.out.println(ds);
+               ans.add(new ArrayList<>(ds));
+           }
+           return ans;
+        }
+        
+        ds.add(arr[i]);
+        s+=arr[i];
+        sum(arr,n,sum,i+1,s,ds,ans);
+        ds.remove(ds.size()-1);
+        s-=arr[i];
+        
+        while(i+1<arr.length && arr[i+1]==arr[i]) ++i;
+        sum(arr,n,sum,i+1,s,ds,ans);
+        
+        return ans;
+    }
+}
