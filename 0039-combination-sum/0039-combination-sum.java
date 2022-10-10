94b2ba2c594cb03196b06cd1bd7df68b67aa739f
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-10 20:30:32 +0530

    Time: 4 ms (82.17%), Space: 44.8 MB (69.19%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        // 2^t*k(len of a ds), no of combinations - k*x
+        Arrays.sort(candidates);
+        return findComb(0, candidates, target, new ArrayList<>(), new ArrayList<>());
+    }
+    
+    private List<List<Integer>> findComb(int index, int[] c, int target, List<List<Integer>> ans, List<Integer> ds) {
+        if(index == c.length || target==0) {
+            if(target==0) {
+                ans.add(new ArrayList<>(ds));
+            }
+            return ans;
+        }
+        
+        // if c[index] > target then it's already useless for us & we call on the next element index+1
+        if(c[index] <= target) {      
+            ds.add(c[index]);
+            findComb(index, c, target-c[index], ans, ds);
+            ds.remove(ds.size()-1);
+        }
+        findComb(index+1, c, target, ans, ds);
+        
+        return ans;
+    }
+}
