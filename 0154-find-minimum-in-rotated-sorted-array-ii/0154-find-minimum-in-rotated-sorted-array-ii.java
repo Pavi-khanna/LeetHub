2bfcadc8e0b174270da2deebe319d5bb0fac5e33
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-14 10:48:01 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (85.86%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
@@ -1,29 +1,42 @@
 class Solution {
     public int findMin(int[] nums) {
-        int start = 0, end = nums.length-1, mid = start + (end-start)/2;
+//         int start = 0, end = nums.length-1, mid = start + (end-start)/2;
         
-        if(nums == null || nums.length == 0) return -1;
+//         if(nums == null || nums.length == 0) return -1;
         
-        while(start <= end) {
-            while (start < end && nums[start] == nums[start + 1])
-                ++start;
-            while (start < end && nums[end] == nums[end - 1])
-                --end;
-            mid = start + (end-start)/2;
+//         while(start <= end) {
+//             while (start < end && nums[start] == nums[start + 1])
+//                 ++start;
+//             while (start < end && nums[end] == nums[end - 1])
+//                 --end;
+//             mid = start + (end-start)/2;
             
-            // we just have to go to the rotated side doesn't matter if its on the left or right bcz the min                element lies on that side only and accd to the graph of rotated sorted array the unsorted                    side is the rotated one.
-            //                      /\    
-            //                     /  \  /
-            // array graph =           \/
+//             // we just have to go to the rotated side doesn't matter if its on the left or right bcz the min                element lies on that side only and accd to the graph of rotated sorted array the unsorted                    side is the rotated one.
+//             //                      /\    
+//             //                     /  \  /
+//             // array graph =           \/
             
             
-            // left side
-            if(nums[mid] < nums[end]) {
-                end = mid;
-            } else { // right side
-                start = mid + 1;
-            }
-        }
-        return nums[mid];
+//             // left side
+//             if(nums[mid] < nums[end]) {
+//                 end = mid;
+//             } else { // right side
+//                 start = mid + 1;
+//             }
+//         }
+//         return nums[mid];
+        
+        int low = 0, high = nums.length - 1;
+
+    while (low < high) {
+      int pivot = low + (high - low) / 2;
+      if (nums[pivot] < nums[high])
+        high = pivot;
+      else if (nums[pivot] > nums[high])
+        low = pivot + 1;
+      else
+        high -= 1;
+    }
+    return nums[low];
     }
 }
