8f6d1378feaa7e286842db90e18374bd698cb735
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-21 21:39:12 +0530

    Time: 5 ms (27.61%), Space: 42.4 MB (20.90%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,15 +1,33 @@
 class Solution {
     public boolean isValid(String s) {
+//         Stack<Character> st = new Stack<>();
+        
+//         for(int i=0; i<s.length(); i++) {
+//             char it = s.charAt(i);
+            
+//             if(it=='(' || it=='{' || it=='[') st.push(it);
+//             else {
+//                 if(st.isEmpty()) return false;
+//                 char top = st.peek();
+//                 if((top == '(' && it==')') || (top == '{' && it=='}') || (top == '[' && it==']')) {
+//                     st.pop();
+//                     continue;
+//                 } else return false;
+//             }
+//         }
+//         return st.isEmpty();
+        
+        Map<Character, Character> map = Map.of('(',')','{','}','[',']');
         Stack<Character> st = new Stack<>();
         
         for(int i=0; i<s.length(); i++) {
             char it = s.charAt(i);
             
-            if(it=='(' || it=='{' || it=='[') st.push(it);
+            if(map.containsKey(it)) st.push(it);
             else {
                 if(st.isEmpty()) return false;
                 char top = st.peek();
-                if((top == '(' && it==')') || (top == '{' && it=='}') || (top == '[' && it==']')) {
+                if(it == map.get(top)) {
                     st.pop();
                     continue;
                 } else return false;
