a4ef06c212ecee6f7d6e1b95174194676dea9ff5
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-23 22:37:11 +0530

    Time: 1 ms (70.34%), Space: 46.1 MB (29.86%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,15 +11,29 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        Set<ListNode> set = new HashSet<>();
+//         Set<ListNode> set = new HashSet<>();
         
-        while(head!=null) {
-            if(set.contains(head)) {
-                return true;
-            }
-            set.add(head);
-            head = head.next;
+//         while(head!=null) {
+//             if(set.contains(head)) {
+//                 return true;
+//             }
+//             set.add(head);
+//             head = head.next;
+//         }
+//         return false;
+        
+        
+        if(head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast.next != null && fast.next.next!=null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(fast == slow) return true;
         }
         return false;
+        
     }
 }
