7e15e3fa1b2b69e6e470503b570102926933095a
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-24 16:30:56 +0530

    Time: 78 ms (5.03%), Space: 43.2 MB (77.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -9,13 +9,25 @@ class Solution {
 //         return Arrays.equals(str1,str2);
         
         
-        int[] counter = new int[26];
+        // int[] counter = new int[26];
+        // for(int i=0; i<s.length(); i++) {
+        //     counter[s.charAt(i)-'a']++;
+        //     counter[t.charAt(i)-'a']--;
+        // }
+        // for(int count : counter) {
+        //     if(count>0) return false;
+        // }
+        // return true;
+        
+        
+        Map<Character, Integer> map = new HashMap<>();
+        
         for(int i=0; i<s.length(); i++) {
-            counter[s.charAt(i)-'a']++;
-            counter[t.charAt(i)-'a']--;
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i),0)-1);
         }
-        for(int count : counter) {
-            if(count>0) return false;
+        for(char c : map.keySet()) {
+            if(map.get(c)!=0) return false;
         }
         return true;
     }
