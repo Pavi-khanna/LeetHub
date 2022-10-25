d361cc0ec6bc70aee6119de62944453d577b4817
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-25 15:12:36 +0530

    Time: 9 ms (24.85%), Space: 43.8 MB (61.07%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -13,49 +13,47 @@ class Solution {
         //                    .toArray();
         
         
-        List<Integer> set = new ArrayList<>();
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int i = 0, j = 0;
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] < nums2[j]) {
-                i++;
-            } else if (nums1[i] > nums2[j]) {
-                j++;
-            } else {
-                set.add(nums1[i]);
-                i++;
-                j++;
+        // List<Integer> set = new ArrayList<>();
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
+        // int i = 0, j = 0;
+        // while (i < nums1.length && j < nums2.length) {
+        //     if (nums1[i] < nums2[j]) {
+        //         i++;
+        //     } else if (nums1[i] > nums2[j]) {
+        //         j++;
+        //     } else {
+        //         set.add(nums1[i]);
+        //         i++;
+        //         j++;
+        //     }
+        // }
+        // // int[] result = new int[set.size()];
+        // // int k = 0;
+        // // for (Integer num : set) {
+        // //     result[k++] = num;
+        // // }
+        // // return result;
+        // return set.stream().mapToInt(Integer::intValue).toArray();
+        
+        
+        
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+        Map<Integer, Integer> m = new HashMap<>();
+        List<Integer> intersection = new ArrayList<>();
+        
+        for(int num : nums1) m.put(num, m.getOrDefault(num, 0)+1);
+        
+        for(int num : nums2) {
+            int count = m.getOrDefault(num, 0);
+            if(count > 0) {
+                intersection.add(num);
+                m.put(num, count-1);
             }
         }
-        // int[] result = new int[set.size()];
-        // int k = 0;
-        // for (Integer num : set) {
-        //     result[k++] = num;
-        // }
-        // return result;
-        return set.stream().mapToInt(Integer::intValue).toArray();
-        
-        
-        
-//         if (nums1.length > nums2.length) {
-//             return intersect(nums2, nums1);
-//         }
-//         Map<Integer, Integer> m = new HashMap<>();
-//         List<Integer> intersection = new ArrayList<>();
-        
-//         for(int num : nums1) m.put(num, m.getOrDefault(num, 0)+1);
-        
-//         for(int num : nums2) {
-//             int count = m.getOrDefault(num, 0);
-//             if(count > 0) {
-//                 intersection.add(num);
-//                 m.put(num, count-1);
-//             }
-//         }
-//         return intersection.stream()
-//                            .mapToInt(Integer::intValue)
-//                            .toArray();
+        return intersection.stream().mapToInt(Integer::intValue).toArray();
     }
     
     public boolean binarySearch(int[] nums, int target) {
