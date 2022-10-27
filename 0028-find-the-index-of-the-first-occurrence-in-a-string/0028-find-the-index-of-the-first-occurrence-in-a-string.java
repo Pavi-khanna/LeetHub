56db28d36de59e404f0d5dfe0722e96385430998
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-27 12:58:23 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (91.32%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,38 +1,41 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-//         int nlen = needle.length();
+        int nlen = needle.length();
         
-//         if(nlen > haystack.length()) return -1;
-//         if(nlen == 0) return 0;
-//         // if(needle.length() == 1) return haystack.indexOf(needle.charAt(0));
+        if(nlen > haystack.length()) return -1;
+        if(nlen == 0) return 0;
+        // if(needle.length() == 1) return haystack.indexOf(needle.charAt(0));
         
-//         for(int i=0; i<haystack.length(); i++) {
-//             if(needle.charAt(0) == haystack.charAt(i)) {
-//                 if(i+nlen <= haystack.length() && needle.equals(haystack.substring(i, i+nlen))) {
-//                     return i;
-//                 }
-//             }
-//         }
-//         return -1;
-        
-        
-        int m = haystack.length(),n = needle.length();
-    if(m < n) return -1;
-    else if(n == 0) return 0;
-    
-    int i = 0;
-    while(i <= m-n) {
-        int j = 0;
-        while (j < n) {
-            if(haystack.charAt(i + j) != needle.charAt(j)) break;
-            
-            j++;
+        for(int i=0; i<haystack.length(); i++) {
+            if(needle.charAt(0) == haystack.charAt(i)) {
+                int j=0;
+                while(j<nlen && i+nlen <= haystack.length()) {
+                    if(needle.charAt(j) != haystack.charAt(i+j)) break;
+                    j++;
+                }
+                if(j==nlen) return i;
+            }
         }
-        if(j == n)
-            return i;
-        i++;
-    }
+        return -1;
+        
+        
+//         int m = haystack.length(),n = needle.length();
+//     if(m < n) return -1;
+//     else if(n == 0) return 0;
     
-    return -1;
+//     int i = 0;
+//     while(i <= m-n) {
+//         int j = 0;
+//         while (j < n) {
+//             if(haystack.charAt(i + j) != needle.charAt(j)) break;
+            
+//             j++;
+//         }
+//         if(j == n)
+//             return i;
+//         i++;
+//     }
+    
+//     return -1;
     }
 }
