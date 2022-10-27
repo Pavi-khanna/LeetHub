ad8f8645637bd16fa3190ac4a4e1323631b1eae8
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-27 13:10:36 +0530

    Time: 1 ms (69.23%), Space: 42.1 MB (36.25%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,41 +1,30 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int nlen = needle.length();
+        int mlen = haystack.length(), nlen = needle.length();
         
         if(nlen > haystack.length()) return -1;
         if(nlen == 0) return 0;
         // if(needle.length() == 1) return haystack.indexOf(needle.charAt(0));
         
-        for(int i=0; i<haystack.length(); i++) {
-            if(needle.charAt(0) == haystack.charAt(i)) {
-                int j=0;
-                while(j<nlen && i+nlen <= haystack.length()) {
-                    if(needle.charAt(j) != haystack.charAt(i+j)) break;
-                    j++;
-                }
-                if(j==nlen) return i;
+        // for(int i=0; i<haystack.length(); i++) {
+        //     if(needle.charAt(0) == haystack.charAt(i)) {
+        //         if(i+nlen <= haystack.length() && needle.equals(haystack.substring(i, i+nlen))) {
+        //             return i;
+        //         }
+        //     }
+        // }
+        
+        int i=0;
+        while(i<= mlen-nlen) {
+            int j=0;
+            
+            while(j<nlen) {
+                if(haystack.charAt(i + j) != needle.charAt(j)) break;
+                j++;
             }
+            if(j==nlen) return i;
+            i++;
         }
         return -1;
-        
-        
-//         int m = haystack.length(),n = needle.length();
-//     if(m < n) return -1;
-//     else if(n == 0) return 0;
-    
-//     int i = 0;
-//     while(i <= m-n) {
-//         int j = 0;
-//         while (j < n) {
-//             if(haystack.charAt(i + j) != needle.charAt(j)) break;
-            
-//             j++;
-//         }
-//         if(j == n)
-//             return i;
-//         i++;
-//     }
-    
-//     return -1;
     }
 }
