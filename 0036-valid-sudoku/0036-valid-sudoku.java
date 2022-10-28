ec7cacb525c2a1b69ecbd569cb938ce2c2b442d6
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-10-28 09:49:48 +0530

    Time: 3 ms (89.52%), Space: 45.9 MB (73.15%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -12,36 +12,49 @@ class Solution {
         //     boxes[r] = new HashSet<Character>();
         // }
         
-        int[][] rows = new int[N][N];
-        int[][] cols = new int[N][N];
-        int[][] boxes = new int[N][N];
+        // int[][] rows = new int[N][N];
+        // int[][] cols = new int[N][N];
+        // int[][] boxes = new int[N][N];
+        
+        int[] rows = new int[N];
+        int[] cols = new int[N];
+        int[] boxes = new int[N];
 
         for (int r = 0; r < N; r++) {
             for (int c = 0; c < N; c++) {
-                char val = board[r][c];
-                if (val == '.') continue;
+                // char val = board[r][c];
+                if (board[r][c] == '.') continue;
                 
-                int pos = board[r][c] - '1';
+                // int pos = board[r][c] - '1';
+                
+                int val = board[r][c] - '0';
+                int pos = 1 << (val-1);
 
                 // row
                 // if (rows[r].contains(val)) return false;
                 // rows[r].add(val);
-                if (rows[r][pos] == 1) return false;
-                rows[r][pos] = 1;
+                // if (rows[r][pos] == 1) return false;
+                // rows[r][pos] = 1;
+                if ((rows[r] & pos) > 0) return false;
+                rows[r] |= pos;
 
                 // column
                 // if (cols[c].contains(val)) return false;
                 // cols[c].add(val);
-                if (cols[c][pos] == 1) return false;
-                cols[c][pos] = 1;
+                // if (cols[c][pos] == 1) return false;
+                // cols[c][pos] = 1;
+                if ((cols[c] & pos) > 0) return false;
+                cols[c] |= pos;
 
                 // box
                 int idx = (r / 3) * 3 + c / 3;
                 // System.out.println(idx);
                 // if (boxes[idx].contains(val)) return false;
                 // boxes[idx].add(val);
-                if (boxes[idx][pos] == 1) return false;
-                boxes[idx][pos] = 1;
+                // if (boxes[idx][pos] == 1) return false;
+                // boxes[idx][pos] = 1;
+                if ((boxes[idx] & pos) > 0) return false;
+                boxes[idx] |= pos;
             }
         }
         return true;
