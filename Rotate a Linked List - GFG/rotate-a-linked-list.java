b8c2ac9e2d371c6d4a59d72b747c2ee70298156b
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2022-12-28 16:26:13 +0530

    Added solution - LeetHub

--- Rotate a Linked List - GFG/rotate-a-linked-list.java
+++ Rotate a Linked List - GFG/rotate-a-linked-list.java
@@ -65,55 +65,55 @@ class Solution{
     //Function to rotate a linked list.
     public Node rotate(Node head, int k) {
         // add code here
-        // if(head==null || head.next==null || k==0) return head;
+        if(head==null || head.next==null || k==0) return head;
 
-        // //https://leetcode.com/problems/rotate-list/discuss/1839864/Java-Thought-Process-Explained-from-Brute-Force-to-Optimized
+        //https://leetcode.com/problems/rotate-list/discuss/1839864/Java-Thought-Process-Explained-from-Brute-Force-to-Optimized
         
-        // // find length
-        // Node cur = head;
-        // int len=1;
-        // while(cur.next!=null) {
-        //     len++;
-        //     cur = cur.next;
-        // }
-        
-        // // go to that node - make it circular
-        // cur.next = head;
-        // k = k%len;
-        // while(k-- >0) cur = cur.next;
-        
-        // // make node head and break connection
-        // head=cur.next;
-        // cur.next = null;
-        
-        // return head;
-        
-        
-        
-        
-        Node x = head; // creating copy of head in x to iterate the list while keeping the head unchanged
-        if( x == null || k==0) return head; // if list is empty or if we don't have to rotate the list, return it
-        int n = 1;  // counting no. of nodes in the list 
-        while( x.next != null){
-            x = x.next;
-            n++;
+        // find length
+        Node cur = head;
+        int len=1;
+        while(cur.next!=null) {
+            len++;
+            cur = cur.next;
         }
-        k = n-k% n; // updating k acc. to point 5
-        while(k-- > 0){
-            Node y = head;
-            int preValue = y.data;
-
-            y = y.next; // starting from 2nd node
-            while( y != null ){
-                // swapping x.data with preValue
-                int temp = y.data;  
-                y.data = preValue;
-                preValue = temp;
-
-                y= y.next;
-            }
-            head.data = preValue; //updating 1st node value to last
-        }
+        
+        // go to that node - make it circular
+        cur.next = head;
+        k = k%len;
+        while(k-- >0) cur = cur.next;
+        
+        // make node head and break connection
+        head=cur.next;
+        cur.next = null;
+        
         return head;
+        
+        
+        
+        
+        // Node x = head; // creating copy of head in x to iterate the list while keeping the head unchanged
+        // if( x == null || k==0) return head; // if list is empty or if we don't have to rotate the list, return it
+        // int n = 1;  // counting no. of nodes in the list 
+        // while( x.next != null){
+        //     x = x.next;
+        //     n++;
+        // }
+        // k = n-k% n; // updating k acc. to point 5
+        // while(k-- > 0){
+        //     Node y = head;
+        //     int preValue = y.data;
+
+        //     y = y.next; // starting from 2nd node
+        //     while( y != null ){
+        //         // swapping x.data with preValue
+        //         int temp = y.data;  
+        //         y.data = preValue;
+        //         preValue = temp;
+
+        //         y= y.next;
+        //     }
+        //     head.data = preValue; //updating 1st node value to last
+        // }
+        // return head;
     }
 }
