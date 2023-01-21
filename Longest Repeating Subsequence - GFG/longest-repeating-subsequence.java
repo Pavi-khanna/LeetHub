47a103332b8e022a4e2f44bdccf20e540e134ae5
Pavi-khanna <32890745+Pavi-khanna@users.noreply.github.com>
2023-01-21 20:53:46 +0530

    Added solution - LeetHub

---
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String str = br.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.LongestRepeatingSubsequence(str);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int LongestRepeatingSubsequence(String str)
+    {
+        String text1 = str, text2 = str;
+        char[] t1 = text1.toCharArray();
+        char[] t2 = text2.toCharArray();
+        
+        // return longestCommonSubsequence(t1, t2, text1.length(), text2.length());
+        
+        int[][] t = new int[text1.length()+1][text2.length()+1];
+        // for(int i=0; i<text1.length()+1; i++) {
+        //     Arrays.fill(t[i],-1);
+        // }
+        // return longestCommonSubsequence(t, t1, t2, text1.length(), text2.length());
+        
+        
+        return longestCommonSubsequence(t, t1, t2);
+    }
+    
+    public int longestCommonSubsequence(char[] t1, char[] t2, int n, int m) {
+        if(n==0 || m==0) return 0;
+        
+        if(t1[n-1] == t2[m-1] && n!=m) {
+            return 1+longestCommonSubsequence(t1, t2, n-1, m-1);
+        } else {
+            return Math.max(longestCommonSubsequence(t1,t2,n-1,m),
+                           longestCommonSubsequence(t1,t2,n,m-1));
+        }
+    }
+    
+    public int longestCommonSubsequence(int[][] t, char[] t1, char[] t2, int n, int m) {
+        if(n==0 || m==0) return 0;
+        
+        if(t[n][m] != -1) return t[n][m];
+        
+        if(t1[n-1] == t2[m-1] && n!=m) {
+            return t[n][m] = 1+longestCommonSubsequence(t, t1, t2, n-1, m-1);
+        } else {
+            return t[n][m] = Math.max(longestCommonSubsequence(t, t1,t2,n-1,m),
+                           longestCommonSubsequence(t, t1,t2,n,m-1));
+        }
+    }
+    
+    public int longestCommonSubsequence(int[][] t, char[] t1, char[] t2) {
+        for(int i=0; i<t.length; i++) t[i][0] = 0;
+        for(int i=0; i<t[0].length; i++) t[0][i] = 0;
+                
+        for(int i=1; i<t.length; i++) {
+            for(int j=1; j<t[0].length; j++) {
+                if(t1[i-1] == t2[j-1] && i!=j) {
+                    t[i][j] = 1+t[i-1][j-1];
+                } else {
+                    t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
+                }
+            }
+        }
+        return t[t.length-1][t[0].length-1];
+    }
+}
